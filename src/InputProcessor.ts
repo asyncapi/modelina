8f0a9e97225baef3bcf1862dbedd2165e57c1804
Jonas Lagoni <jonas-lt@live.dk>
2021-01-25 17:27:48 +0100

    Converted to generic main input processor.

--- src/InputProcessor.ts
+++ src/InputProcessor.ts
@@ -1,28 +1,37 @@
-
-import { AbstractInputProcessor } from "models/AbstractInputProcessor";
-import { CommonInputModel } from "models/CommonInputModel";
-import { AsyncAPIInputProcessor } from "processors/AsyncAPIInputProcessor";
-import { JsonSchemaInputProcessor } from "processors/JsonSchemaInputProcessor";
+import { AbstractInputProcessor } from "./models/AbstractInputProcessor";
+import { CommonInputModel } from "./models/CommonInputModel";
 
 /**
  * Main input processor which figures out the type of input it receives and delegates the processing into separate individual processors.
  */
 export class InputProcessor extends AbstractInputProcessor {
-  asyncAPIProcessor: AbstractInputProcessor;
-  jsonSchemaProcessor: AbstractInputProcessor;
+  processors: Map<string, AbstractInputProcessor> = new Map();
 
   constructor(){
     super();
-    this.asyncAPIProcessor = new AsyncAPIInputProcessor();
-    this.jsonSchemaProcessor = new JsonSchemaInputProcessor();
   }
 
-  async process(object: any): Promise<CommonInputModel> {
-    if(AsyncAPIInputProcessor.isAsyncAPI(object)){
-      return await this.asyncAPIProcessor.process(object);
-    } else {
-      //Default to processing input as JSON Schema
-      return await this.jsonSchemaProcessor.process(object);
+  /**
+   * Add a processor.
+   * 
+   * @param type of processor
+   * @param processor
+   */
+  addProcessor(type: string, processor: AbstractInputProcessor) {
+    this.processors.set(type, processor);
+  }
+
+  /**
+   * The processor code which delegates the processing to the correct implementation.
+   * 
+   * @param object to process
+   * @param type of processor to use
+   */
+  process(object: any, type: string = 'json-schema'): Promise<CommonInputModel> {
+    const processor = this.processors.get(type);
+    if (processor === undefined) {
+      throw Error(`Could not find processor '${type}'`);
     }
+    return processor.process(object);
   }
 }
--- test/InputProcessor.spec.ts
+++ test/InputProcessor.spec.ts
@@ -1,76 +1,82 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import {parse} from '@asyncapi/parser';
-import { JsonSchemaInputProcessor } from '../src/processors/JsonSchemaInputProcessor';
+import { InputProcessor } from '../src/InputProcessor';
 import { AsyncAPIInputProcessor } from '../src/processors/AsyncAPIInputProcessor';
+import { JsonSchemaInputProcessor } from '../src/processors/JsonSchemaInputProcessor';
 describe('InputProcessor', function() {
-    describe('process()', function() {
+    let mainProcessor : InputProcessor;
+    beforeAll(() => {
+        mainProcessor = new InputProcessor();
+        const asyncAPI = new AsyncAPIInputProcessor();
+        mainProcessor.addProcessor("asyncapi", asyncAPI); 
+        const jsonSchema = new JsonSchemaInputProcessor();
+        mainProcessor.addProcessor("json-schema", jsonSchema);
+    });
 
-        /**
-         * The input schema when processed should be equals to the expected CommonInputModel
-         * 
-         * @param inputSchemaPath 
-         * @param expectedCommonModulePath 
-         */
-        const expectFunction = async (inputSchemaPath: string, expectedCommonModulePath: string) => {
-            const inputSchemaString = fs.readFileSync(path.resolve(__dirname, inputSchemaPath), 'utf8');
-            const expectedCommonInputModelString = fs.readFileSync(path.resolve(__dirname, expectedCommonModulePath), 'utf8');
-            const inputSchema = JSON.parse(inputSchemaString);
-            const expectedCommonInputModel = JSON.parse(expectedCommonInputModelString);
-            const processor = new JsonSchemaInputProcessor();
-            const commonInputModel = await processor.process(inputSchema);
-            expect(commonInputModel).toEqual(expectedCommonInputModel);
-        }
+    /**
+     * The input schema when processed should be equals to the expected CommonInputModel
+     * 
+     * @param inputSchemaPath 
+     * @param expectedCommonModulePath 
+     */
+    const expectFunction = async (inputSchemaPath: string, expectedCommonModulePath: string, type: string) => {
+        const inputSchemaString = fs.readFileSync(path.resolve(__dirname, inputSchemaPath), 'utf8');
+        const expectedCommonInputModelString = fs.readFileSync(path.resolve(__dirname, expectedCommonModulePath), 'utf8');
+        const inputSchema = JSON.parse(inputSchemaString);
+        const expectedCommonInputModel = JSON.parse(expectedCommonInputModelString);
+        const commonInputModel = await mainProcessor.process(inputSchema, type);
+        expect(commonInputModel).toEqual(expectedCommonInputModel);
+    }
+    describe('process()', function() {
         describe('should be able to process JSON schema input', function() {
             test('with absence types', async function() {
-                const inputSchemaPath = './JsonSchemaInputProcessor/absence_type.json';
-                const expectedCommonModulePath = './JsonSchemaInputProcessor/commonInputModel/absence_type.json';
-                await expectFunction(inputSchemaPath, expectedCommonModulePath);
+                const inputSchemaPath = './processors/JsonSchemaInputProcessor/absence_type.json';
+                const expectedCommonModulePath = './processors/JsonSchemaInputProcessor/commonInputModel/absence_type.json';
+                await expectFunction(inputSchemaPath, expectedCommonModulePath, "json-schema");
             });
             test('with conditional schemas', async function() {
-                const inputSchemaPath = './JsonSchemaInputProcessor/applying_conditional_schemas.json';
-                const expectedCommonModulePath = './JsonSchemaInputProcessor/commonInputModel/applying_conditional_schemas.json';
-                await expectFunction(inputSchemaPath, expectedCommonModulePath);
+                const inputSchemaPath = './processors/JsonSchemaInputProcessor/applying_conditional_schemas.json';
+                const expectedCommonModulePath = './processors/JsonSchemaInputProcessor/commonInputModel/applying_conditional_schemas.json';
+                await expectFunction(inputSchemaPath, expectedCommonModulePath, "json-schema");
             });
             test('with combination schemas', async function() {
-                const inputSchemaPath = './JsonSchemaInputProcessor/combination_schemas.json';
-                const expectedCommonModulePath = './JsonSchemaInputProcessor/commonInputModel/combination_schemas.json';
-                await expectFunction(inputSchemaPath, expectedCommonModulePath);
+                const inputSchemaPath = './processors/JsonSchemaInputProcessor/combination_schemas.json';
+                const expectedCommonModulePath = './processors/JsonSchemaInputProcessor/commonInputModel/combination_schemas.json';
+                await expectFunction(inputSchemaPath, expectedCommonModulePath, "json-schema");
             });
             test('with enum schemas', async function() {
-                const inputSchemaPath = './JsonSchemaInputProcessor/enum.json';
-                const expectedCommonModulePath = './JsonSchemaInputProcessor/commonInputModel/enum.json';
-                await expectFunction(inputSchemaPath, expectedCommonModulePath);
+                const inputSchemaPath = './processors/JsonSchemaInputProcessor/enum.json';
+                const expectedCommonModulePath = './processors/JsonSchemaInputProcessor/commonInputModel/enum.json';
+                await expectFunction(inputSchemaPath, expectedCommonModulePath, "json-schema");
             });
             test('with items schemas', async function() {
-                const inputSchemaPath = './JsonSchemaInputProcessor/items.json';
-                const expectedCommonModulePath = './JsonSchemaInputProcessor/commonInputModel/items.json';
-                await expectFunction(inputSchemaPath, expectedCommonModulePath);
+                const inputSchemaPath = './processors/JsonSchemaInputProcessor/items.json';
+                const expectedCommonModulePath = './processors/JsonSchemaInputProcessor/commonInputModel/items.json';
+                await expectFunction(inputSchemaPath, expectedCommonModulePath, "json-schema");
             });
             test('with multiple objects', async function() {
-                const inputSchemaPath = './JsonSchemaInputProcessor/multiple_objects.json';
-                const expectedCommonModulePath = './JsonSchemaInputProcessor/commonInputModel/multiple_objects.json';
-                await expectFunction(inputSchemaPath, expectedCommonModulePath);
+                const inputSchemaPath = './processors/JsonSchemaInputProcessor/multiple_objects.json';
+                const expectedCommonModulePath = './processors/JsonSchemaInputProcessor/commonInputModel/multiple_objects.json';
+                await expectFunction(inputSchemaPath, expectedCommonModulePath, "json-schema");
             });
         });
 
         describe('should be able to process AsyncAPI schema input', function() {
             test('with pure object', async function() {
-                const basicDocString = fs.readFileSync(path.resolve(__dirname, './AsyncAPIInputProcessor/basic.json'), 'utf8');
-                const expectedCommonInputModelString = fs.readFileSync(path.resolve(__dirname, './AsyncAPIInputProcessor/commonInputModel/basic.json'), 'utf8');
+                const basicDocString = fs.readFileSync(path.resolve(__dirname, './processors/AsyncAPIInputProcessor/basic.json'), 'utf8');
+                const expectedCommonInputModelString = fs.readFileSync(path.resolve(__dirname, './processors/AsyncAPIInputProcessor/commonInputModel/basic.json'), 'utf8');
                 const basicDoc = JSON.parse(basicDocString);
                 const expectedCommonInputModel = JSON.parse(expectedCommonInputModelString);
-                const processor = new AsyncAPIInputProcessor();
-                const commonInputModel = await processor.process(basicDoc);
+                const commonInputModel = await mainProcessor.process(basicDoc);
                 expect(commonInputModel).toEqual(expectedCommonInputModel);
             });
             test('with parsed document', async function() {
-                const basicDocString = fs.readFileSync(path.resolve(__dirname, './AsyncAPIInputProcessor/basic.json'), 'utf8');
-                const expectedCommonInputModelString = fs.readFileSync(path.resolve(__dirname, './AsyncAPIInputProcessor/commonInputModel/basic.json'), 'utf8');
+                const basicDocString = fs.readFileSync(path.resolve(__dirname, './processors/AsyncAPIInputProcessor/basic.json'), 'utf8');
+                const expectedCommonInputModelString = fs.readFileSync(path.resolve(__dirname, './processors/AsyncAPIInputProcessor/commonInputModel/basic.json'), 'utf8');
                 const expectedCommonInputModel = JSON.parse(expectedCommonInputModelString);
                 const parsedObject = await parse(basicDocString);
-                const processor = new AsyncAPIInputProcessor();
-                const commonInputModel = await processor.process(parsedObject);
+                const commonInputModel = await mainProcessor.process(parsedObject);
                 expect(commonInputModel).toEqual(expectedCommonInputModel);
             });
         });
