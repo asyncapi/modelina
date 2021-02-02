e740e14d980a65b38b874df60741b7fbd67b9503
Jonas Lagoni <jonas-lt@live.dk>
2021-02-02 15:11:05 +0100

    Added simplify tests

---
+++ test/simplification/simplify.spec.ts
@@ -0,0 +1,20 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import {simplify} from '../../src/simplification/Simplify';
+
+/**
+ * Some of these test are purely theoretical and have little if any merit 
+ * on a JSON Schema which actually makes sense but are used to test the principles.
+ */
+describe('Simplification', function() {
+  test('should return as is', function() {
+    const inputSchemaString = fs.readFileSync(path.resolve(__dirname, './simplify/multipleObjects.json'), 'utf8');
+    const expectedSchemaString = fs.readFileSync(path.resolve(__dirname, './simplify/expected/multipleObjects.json'), 'utf8');
+    const schema = JSON.parse(inputSchemaString);
+    const expectedModels = JSON.parse(expectedSchemaString);
+    const actualModels = simplify(schema);
+    expect(actualModels).not.toBeUndefined();
+    expect(actualModels[0]).toEqual(expectedModels[1]);
+    expect(actualModels[1]).toEqual(expectedModels[0]);
+  });
+});
