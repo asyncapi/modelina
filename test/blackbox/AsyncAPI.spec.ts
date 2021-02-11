5f8600e8c1e0c5bf1444d105fca9e541fceb2386
Jonas Lagoni <jonas-lt@live.dk>
2021-02-11 16:07:57 +0100

    Added blackbox test

---
+++ test/blackbox/AsyncAPI.spec.ts
@@ -0,0 +1,14 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { JsonSchemaInputProcessor } from '../../src/processors/JsonSchemaInputProcessor';
+describe('AsyncAPI JSON Schema file', function() {
+    test('should be simplified', async function() {
+      const inputSchemaString = fs.readFileSync(path.resolve(__dirname, './AsyncAPI/AsyncAPI.json'), 'utf8');
+      const expectedSchemaString = fs.readFileSync(path.resolve(__dirname, './AsyncAPI/expected/AsyncAPI.json'), 'utf8');
+      const inputSchema = JSON.parse(inputSchemaString);
+      const expectedCommonInputModel = JSON.parse(expectedSchemaString);
+      const processor = new JsonSchemaInputProcessor();
+      const commonInputModel = await processor.process(inputSchema);
+      expect(commonInputModel).toEqual(expectedCommonInputModel);
+    });
+});
