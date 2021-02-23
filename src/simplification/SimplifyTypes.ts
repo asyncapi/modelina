7aaa87d416944486d5deed8e06e39c7647503c81
Jonas Lagoni <jonas-lt@live.dk>
2021-02-23 16:12:00 +0100

    chore: require eslint warnings are addressed (#81)

--- src/simplification/SimplifyTypes.ts
+++ src/simplification/SimplifyTypes.ts
@@ -15,103 +15,155 @@ export default function simplifyTypes(schema: Schema | boolean, seenSchemas: Map
     } 
     throw new Error('False value schemas are not supported');
   }
-  let types: Output = [];
+  const types: Output = [];
   if (seenSchemas.has(schema)) return seenSchemas.get(schema);
   seenSchemas.set(schema, types);
-  const addToTypes = (typesToCheck: Output) => {
-    if (typesToCheck !== undefined) {
-      if (Array.isArray(typesToCheck)) {
-        typesToCheck.forEach(addToTypes);
-      } else if (Array.isArray(types) && !types.includes(typesToCheck)) {
-        types.push(typesToCheck);
-      }
-    }
-  };
-  const handler = (schemas: (Schema | boolean)[] = []) => {
-    schemas.forEach((schema) => {
-      addToTypes(simplifyTypes(schema, seenSchemas));
-    });
-  };
 
-  if (schema.type) {
-    addToTypes(schema.type);
-  }
+  addToTypes(schema.type, types);
+
   //If we encounter combination schemas ensure we recursively find the types
-  handler(schema.allOf);
-  handler(schema.oneOf);
-  handler(schema.anyOf);
+  combineSchemas(schema.allOf, types, seenSchemas);
+  combineSchemas(schema.oneOf, types, seenSchemas);
+  combineSchemas(schema.anyOf, types, seenSchemas);
 
   //Infer types from items and properties
   if (schema.items !== undefined) {
-    addToTypes('array');
+    addToTypes('array', types);
   }
   if (schema.properties !== undefined) {
-    addToTypes('object');
+    addToTypes('object', types);
   }
 
   //If we encounter combination schemas ensure we recursively find and cumulate the types
-  if (schema.then) {
-    addToTypes(simplifyTypes(schema.then, seenSchemas));
-  }
-  if (schema.else) {
-    addToTypes(simplifyTypes(schema.else, seenSchemas));
-  }
+  combineSchemas(schema.then, types, seenSchemas);
+  combineSchemas(schema.else, types, seenSchemas);
 
   //Check enums and const keywords to infer type if type has not already been defined.
   if (!schema.type) {
-    const inferTypeFromValue = (value: any) => {
-      if (Array.isArray(value)) {
-        return 'array';
-      }
-      if (value === null) {
-        return 'null';
-      }
-      const typeOfEnum = typeof value;
-      if (typeOfEnum === 'bigint') {
-        return 'number';
-      } 
-      return typeOfEnum;
-    };
-    if (schema.enum) {
-      schema.enum.forEach((value: any) => {
-        const inferredType = inferTypeFromValue(value);
-        if (inferredType !== undefined) {
-          addToTypes(inferredType);
-        }
-      });
-    }
-    //Should const overwrite the type?
-    if (schema.const !== undefined) {
-      const inferredType = inferTypeFromValue(schema.const);
-      if (inferredType !== undefined) {
-        types = inferredType;
-      }
-    }
+    inferTypes(schema, types); 
   }
 
-  //Infer which types should not be there and include what is left, overwrites what ever has been defined earlier.
-  if (schema.not) {
-    const notTypes = simplifyTypes(schema.not, seenSchemas);
-    const remainingTypes = ['object', 'string', 'number', 'array', 'boolean', 'null'];
-    const tryAndCutRemainingArray = (notType: string | undefined) => {
-      if (notType !== undefined && remainingTypes.includes(notType)) {
-        remainingTypes.splice(remainingTypes.indexOf(notType), 1);
-      }
-    };
-    if (Array.isArray(notTypes)) {
-      notTypes.forEach((notType) => {
-        tryAndCutRemainingArray(notType);
-      });
-    } else {
-      tryAndCutRemainingArray(notTypes);
-    }
-    //Assign all remaining types
-    types = remainingTypes;
-  }
+  //Infer which types should not be there and include what is left.
+  inferNotTypes(schema, types, seenSchemas);
+
+  //Ensure we return the correct format of output
   if (types.length === 0) {
     return undefined;
   } else if (types.length === 1) {
     return types[0];
   }
   return types;
+}
+
+/**
+ * Infers the JSON Schema type from value
+ * 
+ * @param value to infer type of
+ */
+function inferTypeFromValue(value: any) {
+  if (Array.isArray(value)) {
+    return 'array';
+  }
+  if (value === null) {
+    return 'null';
+  }
+  const typeOfEnum = typeof value;
+  if (typeOfEnum === 'bigint') {
+    return 'integer';
+  } 
+  return typeOfEnum;
+}
+
+/**
+ * Infer types from enum and const values.
+ * 
+ * @param schema to go through
+ * @param currentOutput the current output
+ */
+function inferTypes(schema: Schema | boolean, currentOutput: Output) {
+  if (schema === undefined || typeof schema === 'boolean' || currentOutput === undefined) return; 
+  if (!Array.isArray(currentOutput)) return;
+  if (schema.enum) {
+    schema.enum.forEach((value: any) => {
+      const inferredType = inferTypeFromValue(value);
+      if (inferredType !== undefined) {
+        addToTypes(inferredType, currentOutput);
+      }
+    });
+  }
+  //Should const overwrite the type?
+  if (schema.const !== undefined) {
+    const inferredType = inferTypeFromValue(schema.const);
+    if (inferredType !== undefined) {
+      currentOutput.length = 0;
+      currentOutput.splice(0, currentOutput.length);
+      currentOutput[0] = inferredType;
+    }
+  }
+}
+
+/**
+ * Infer which types the model should NOT be. 
+ * 
+ * @param schema to go through
+ * @param currentOutput the current output
+ * @param seenSchemas schemas which we already have outputs for
+ */
+function inferNotTypes(schema: Schema | boolean, currentOutput: Output, seenSchemas: Map<any, Output>) {
+  if (schema === undefined || currentOutput === undefined) return;
+  if (!Array.isArray(currentOutput)) return;
+  if (currentOutput.length === 0) currentOutput.push('object', 'string', 'number', 'array', 'boolean', 'null');
+  if (typeof schema === 'boolean') return;
+  if (schema.not) {
+    const notTypes = simplifyTypes(schema.not, seenSchemas);
+    // Cut any not types from the existing output.
+    const tryAndCutRemainingArray = (notType: string | string[] | undefined) => {
+      if (notType === undefined) return;
+      if (Array.isArray(notType)) {
+        notType.forEach((notType) => {
+          tryAndCutRemainingArray(notType);
+        });
+      } else if (currentOutput.includes(notType)) {
+        currentOutput.splice(currentOutput.indexOf(notType), 1);
+      }
+    };
+    tryAndCutRemainingArray(notTypes);
+  }
+}
+
+/**
+ * Adds missing types to the array.
+ * 
+ * @param typesToAdd which types we should try and add to the existing output
+ * @param currentOutput the current output
+ */
+function addToTypes(typesToAdd: Output, currentOutput: Output) {
+  if (typesToAdd !== undefined) {
+    if (Array.isArray(typesToAdd)) {
+      typesToAdd.forEach((value) => {
+        addToTypes(value, currentOutput);
+      });
+    } else if (Array.isArray(currentOutput) && !currentOutput.includes(typesToAdd)) {
+      currentOutput.push(typesToAdd);
+    }
+  }
+}
+
+/**
+ * Go through schema(s) and combine the simplified types together.
+ * 
+ * @param schema to go through
+ * @param currentOutput the current output
+ * @param seenSchemas schemas which we already have outputs for
+ */
+function combineSchemas(schema: (Schema | boolean) | (Schema | boolean)[] | undefined, currentOutput: Output, seenSchemas: Map<any, Output>) {
+  if (schema === undefined) return;
+  if (typeof schema === 'boolean') return;
+  if (Array.isArray(schema)) {
+    schema.forEach((itemSchema) => {
+      combineSchemas(itemSchema, currentOutput, seenSchemas);
+    });
+  } else {
+    addToTypes(simplifyTypes(schema, seenSchemas), currentOutput);
+  }
 }
