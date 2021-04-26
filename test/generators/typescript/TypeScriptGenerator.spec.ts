4c8c4a730dac808d45a945324933bd38b7173fe2
jonas lagoni <jonas-lt@live.dk>
2021-04-26 15:06:47 +0200

    Fixed tests

--- test/generators/typescript/TypeScriptGenerator.spec.ts
+++ test/generators/typescript/TypeScriptGenerator.spec.ts
@@ -14,16 +14,16 @@ describe('TypeScriptGenerator', function() {
       }
     };
     const expected = `export class Address {
-private _state?: "Texas" | "Alabama" | "California" | "other";
+  private _state?: "Texas" | "Alabama" | "California" | "other";
 
-constructor(input: {
-  state?: "Texas" | "Alabama" | "California" | "other",
-}) {
-  this._state = input.state;
-}
+  constructor(input: {
+    state?: "Texas" | "Alabama" | "California" | "other",
+  }) {
+    this._state = input.state;
+  }
 
-get state(): "Texas" | "Alabama" | "California" | "other" | undefined { return this._state; }
-set state(state: "Texas" | "Alabama" | "California" | "other" | undefined) { this._state = state; }
+  get state(): "Texas" | "Alabama" | "California" | "other" | undefined { return this._state; }
+  set state(state: "Texas" | "Alabama" | "California" | "other" | undefined) { this._state = state; }
 }`;
 
     const inputModel = await generator.process(doc);
@@ -51,52 +51,52 @@ set state(state: "Texas" | "Alabama" | "California" | "other" | undefined) { thi
       required: ["street_name", "city", "state", "house_number", "array_type"],
     };
     const expected = `export class Address {
-private _streetName: string;
-private _city: string;
-private _state: string;
-private _houseNumber: number;
-private _marriage?: boolean;
-private _members?: string | number | boolean;
-private _arrayType: Array<string | number>;
+  private _streetName: string;
+  private _city: string;
+  private _state: string;
+  private _houseNumber: number;
+  private _marriage?: boolean;
+  private _members?: string | number | boolean;
+  private _arrayType: Array<string | number>;
 
-constructor(input: {
-  streetName: string,
-  city: string,
-  state: string,
-  houseNumber: number,
-  marriage?: boolean,
-  members?: string | number | boolean,
-  arrayType: Array<string | number>,
-}) {
-  this._streetName = input.streetName;
-  this._city = input.city;
-  this._state = input.state;
-  this._houseNumber = input.houseNumber;
-  this._marriage = input.marriage;
-  this._members = input.members;
-  this._arrayType = input.arrayType;
-}
+  constructor(input: {
+    streetName: string,
+    city: string,
+    state: string,
+    houseNumber: number,
+    marriage?: boolean,
+    members?: string | number | boolean,
+    arrayType: Array<string | number>,
+  }) {
+    this._streetName = input.streetName;
+    this._city = input.city;
+    this._state = input.state;
+    this._houseNumber = input.houseNumber;
+    this._marriage = input.marriage;
+    this._members = input.members;
+    this._arrayType = input.arrayType;
+  }
 
-get streetName(): string { return this._streetName; }
-set streetName(streetName: string) { this._streetName = streetName; }
+  get streetName(): string { return this._streetName; }
+  set streetName(streetName: string) { this._streetName = streetName; }
 
-get city(): string { return this._city; }
-set city(city: string) { this._city = city; }
+  get city(): string { return this._city; }
+  set city(city: string) { this._city = city; }
 
-get state(): string { return this._state; }
-set state(state: string) { this._state = state; }
+  get state(): string { return this._state; }
+  set state(state: string) { this._state = state; }
 
-get houseNumber(): number { return this._houseNumber; }
-set houseNumber(houseNumber: number) { this._houseNumber = houseNumber; }
+  get houseNumber(): number { return this._houseNumber; }
+  set houseNumber(houseNumber: number) { this._houseNumber = houseNumber; }
 
-get marriage(): boolean | undefined { return this._marriage; }
-set marriage(marriage: boolean | undefined) { this._marriage = marriage; }
+  get marriage(): boolean | undefined { return this._marriage; }
+  set marriage(marriage: boolean | undefined) { this._marriage = marriage; }
 
-get members(): string | number | boolean | undefined { return this._members; }
-set members(members: string | number | boolean | undefined) { this._members = members; }
+  get members(): string | number | boolean | undefined { return this._members; }
+  set members(members: string | number | boolean | undefined) { this._members = members; }
 
-get arrayType(): Array<string | number> { return this._arrayType; }
-set arrayType(arrayType: Array<string | number>) { this._arrayType = arrayType; }
+  get arrayType(): Array<string | number> { return this._arrayType; }
+  set arrayType(arrayType: Array<string | number>) { this._arrayType = arrayType; }
 }`;
 
     const inputModel = await generator.process(doc);
@@ -118,17 +118,17 @@ set arrayType(arrayType: Array<string | number>) { this._arrayType = arrayType; 
       }
     };
     const expected = `export class CustomClass {
-@JsonProperty("property")
-private _property?: string;
+  @JsonProperty("property")
+  private _property?: string;
 
-constructor(input: {
-  property?: string,
-}) {
-  this._property = input.property;
-}
+  constructor(input: {
+    property?: string,
+  }) {
+    this._property = input.property;
+  }
 
-get property(): string | undefined { return this._property; }
-set property(property: string | undefined) { this._property = property; }
+  get property(): string | undefined { return this._property; }
+  set property(property: string | undefined) { this._property = property; }
 }`;
 
     generator = new TypeScriptGenerator({ presets: [
