cda16af87fcf02534dacd53fc236dc02b1b09caa
Jonas Lagoni <jonas-lt@live.dk>
2021-04-28 11:41:23 +0200

    Switched check to make it more clear

--- src/simplification/SimplifyProperties.ts
+++ src/simplification/SimplifyProperties.ts
@@ -36,7 +36,7 @@ export default function simplifyProperties(schema: Schema | boolean, simplifier 
     combineSchemas(schema.then, output, simplifier, seenSchemas, schema);
     combineSchemas(schema.else, output, simplifier, seenSchemas, schema);
 
-    return !Object.keys(output).length ? undefined : output;
+    return Object.keys(output).length ? output : undefined;
   }
   return undefined;
 }
