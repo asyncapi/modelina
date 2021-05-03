e5ef3c932d637dec8c7cbdb5f2ed96b68ec0bdf2
Jonas Lagoni <jonas-lt@live.dk>
2021-05-03 18:11:29 +0200

    Added java blackbox test

---
+++ test/CI-blackbox/java/renderer.js
@@ -0,0 +1,22 @@
+
+const {JavaGenerator} = require('../../../lib');
+const fs = require('fs');
+const path = require('path');
+/**
+ * Render all schema models
+ * @returns 
+ */
+async function renderModels() {
+  const input = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../test.json')));
+  const generator = new JavaGenerator();
+  const generatedModels = await generator.generate(input);
+  const outputDir = path.resolve(__dirname, './output');
+  await fs.promises.mkdir(outputDir, { recursive: true });
+  const output = [];
+  for (const generatedModel of generatedModels) {
+    output.push(generatedModel.result);
+  }
+  const outputFile = path.resolve(outputDir, 'output.java');
+  await fs.promises.writeFile(outputFile, output.join('\n'));
+}
+renderModels().then(console.log("Done"));
