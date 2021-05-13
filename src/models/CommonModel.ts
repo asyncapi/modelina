10250e8338ddbcf8222f68e0bed0c6512c863a99
Jonas Lagoni <jonas-lt@live.dk>
2021-05-13 11:02:37 +0200

    refactor: add support functions for CommonModel (#183)
    Co-authored-by: Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>

--- src/models/CommonModel.ts
+++ src/models/CommonModel.ts
@@ -21,196 +21,54 @@ export class CommonModel extends CommonSchema<CommonModel> {
   originalSchema?: Schema | boolean;
   
   /**
-   * Transform object into a type of CommonModel.
-   * 
-   * @param object to transform
-   * @returns CommonModel instance of the object
-   */
-  static toCommonModel(object: unknown): CommonModel {
-    let newCommonModel = new CommonModel();
-    newCommonModel = Object.assign(newCommonModel, object);
-    newCommonModel = CommonSchema.transformSchema(newCommonModel, CommonModel.toCommonModel) as CommonModel;
-    if (newCommonModel.originalSchema !== undefined) {
-      newCommonModel.originalSchema = Schema.toSchema(newCommonModel.originalSchema);
-    }
-    return newCommonModel;
-  }
-  /**
-   * Merge two common model properties together 
-   * @param mergeTo 
-   * @param mergeFrom 
-   * @param originalSchema 
-   */
-  private static mergeProperties(mergeTo: CommonModel, mergeFrom: CommonModel, originalSchema: Schema) {
-    const mergeToProperties = mergeTo.properties;
-    const mergeFromProperties = mergeFrom.properties;
-    if (mergeFromProperties !== undefined) {
-      if (mergeToProperties === undefined) {
-        mergeTo.properties = mergeFromProperties;
-      } else {
-        for (const [propName, prop] of Object.entries(mergeFromProperties)) {
-          if (mergeToProperties[`${propName}`] !== undefined) {
-            mergeToProperties[`${propName}`] = CommonModel.mergeCommonModels(mergeToProperties[`${propName}`], prop, originalSchema);
-          } else {
-            mergeToProperties[`${propName}`] = prop;
-          }
-        }
-      }
-    }
-  }
-  /**
-   * Merge two common model additional properties together 
-   * @param mergeTo 
-   * @param mergeFrom 
-   * @param originalSchema 
-   */
-  private static mergeAdditionalProperties(mergeTo: CommonModel, mergeFrom: CommonModel, originalSchema: Schema) {
-    const mergeToAdditionalProperties = mergeTo.additionalProperties;
-    const mergeFromAdditionalProperties = mergeFrom.additionalProperties;
-    if (mergeFromAdditionalProperties !== undefined) {
-      if (mergeToAdditionalProperties === undefined) {
-        mergeTo.additionalProperties = mergeFromAdditionalProperties;
-      } else {
-        mergeTo.additionalProperties = CommonModel.mergeCommonModels(mergeToAdditionalProperties, mergeFromAdditionalProperties, originalSchema);
-      }
-    }
-  }
-  /**
-   * Merge two common model pattern properties together 
-   * @param mergeTo 
-   * @param mergeFrom 
-   * @param originalSchema 
-   */
-  private static mergePatternProperties(mergeTo: CommonModel, mergeFrom: CommonModel, originalSchema: Schema) {
-    const mergeToPatternProperties = mergeTo.patternProperties;
-    const mergeFromPatternProperties = mergeFrom.patternProperties;
-    if (mergeFromPatternProperties !== undefined) {
-      if (mergeToPatternProperties === undefined) {
-        mergeTo.patternProperties = mergeFromPatternProperties;
-      } else {
-        for (const [pattern, patternModel] of Object.entries(mergeFromPatternProperties)) {
-          if (mergeToPatternProperties[`${pattern}`] !== undefined) {
-            mergeToPatternProperties[`${pattern}`] = CommonModel.mergeCommonModels(mergeToPatternProperties[`${pattern}`], patternModel, originalSchema);
-          } else {
-            mergeToPatternProperties[`${pattern}`] = patternModel;
-          }
-        }
-      }
-    }
-  }
-
-  /**
-   * Merge items together so only one CommonModel remains.
-   * 
-   * @param mergeTo CommonModel to merge types into
-   * @param mergeFrom CommonModel to merge from
-   * @param originalSchema 
-   */
-  private static mergeItems(mergeTo: CommonModel, mergeFrom: CommonModel, originalSchema: Schema) {
-    const merge = (models: CommonModel | CommonModel[] | undefined): CommonModel | undefined => {
-      if (Array.isArray(models)) {
-        if (models.length > 0) {
-          let mergedItemsModel: CommonModel = models[0];
-          models.forEach((model) => { mergedItemsModel = CommonModel.mergeCommonModels(mergedItemsModel, model, originalSchema); });
-          return mergedItemsModel;
-        } 
-        return undefined;
-      }
-      return models;
-    };
-    if (mergeFrom.items !== undefined) {
-      //Incase of arrays, merge them into a single schema
-      const mergeFromItemsModel = merge(mergeFrom.items);
-      const mergeToItemsModel = merge(mergeTo.items);
-      if (mergeFromItemsModel !== undefined) {
-        if (mergeToItemsModel !== undefined) {
-          mergeTo.items = CommonModel.mergeCommonModels(mergeToItemsModel, mergeFromItemsModel, originalSchema);
-        } else {
-          mergeTo.items = mergeFromItemsModel;
-        }
-      }
-    } else if (mergeTo.items !== undefined) {
-      mergeTo.items = merge(mergeTo.items);
-    }
-  }
-
-  /**
-   * Merge types together
-   * 
-   * @param mergeTo CommonModel to merge types into
-   * @param mergeFrom CommonModel to merge from
-   */
-  private static mergeTypes(mergeTo: CommonModel, mergeFrom: CommonModel) {
-    //Only add the types that do not already exist
-    const addToType = (type: string) => {
-      if (mergeTo.type !== undefined && !mergeTo.type.includes(type)) {
-        if (Array.isArray(mergeTo.type)) {
-          mergeTo.type.push(type);
-        } else {
-          mergeTo.type = [mergeTo.type, type];
-        }
-      }
-    };
-    if (mergeFrom.type !== undefined) {
-      if (mergeTo.type === undefined) {
-        mergeTo.type = mergeFrom.type;
-      } else {
-        if (Array.isArray(mergeFrom.type)) {
-          mergeFrom.type.forEach(addToType);
-          return;
-        }
-        addToType(mergeFrom.type);
-      }
-    }
-  }
-
-  /**
-   * Only merge if left side is undefined and right side is sat OR both sides are defined
-   * 
-   * @param mergeTo CommonModel to merge into
-   * @param mergeFrom CommonModel to merge values from
-   * @param originalSchema schema to use as original schema
-   */
-  static mergeCommonModels(mergeTo: CommonModel | undefined, mergeFrom: CommonModel, originalSchema: Schema): CommonModel {
-    if (mergeTo === undefined) return mergeFrom;
-
-    CommonModel.mergeAdditionalProperties(mergeTo, mergeFrom, originalSchema);
-    CommonModel.mergePatternProperties(mergeTo, mergeFrom, originalSchema);
-    CommonModel.mergeProperties(mergeTo, mergeFrom, originalSchema);
-    CommonModel.mergeItems(mergeTo, mergeFrom, originalSchema);
-    CommonModel.mergeTypes(mergeTo, mergeFrom);
-
-    if (mergeFrom.enum !== undefined) {
-      mergeTo.enum = [... new Set([...(mergeTo.enum || []), ...mergeFrom.enum])];
-    }
-    if (mergeFrom.required !== undefined) {
-      mergeTo.required = [... new Set([...(mergeTo.required || []), ...mergeFrom.required])];
-    }
-
-    // Which values are correct to use here? Is allOf required?
-    if (mergeFrom.$id !== undefined) {
-      mergeTo.$id = mergeFrom.$id;
-    }
-    if (mergeFrom.$ref !== undefined) {
-      mergeTo.$ref = mergeFrom.$ref;
-    }
-    if (mergeFrom.extend !== undefined) {
-      mergeTo.extend = mergeFrom.extend;
-    }
-    mergeTo.originalSchema = originalSchema;
-    return mergeTo;
-  }
-
-  /**
    * Retrieves data from originalSchema by given key
    * 
    * @param key given key
    * @returns {any}
    */
   getFromSchema<K extends keyof Schema>(key: K) {
-    let schema = this.originalSchema || {};
-    if (typeof schema === 'boolean') schema = {};
+    const schema = this.originalSchema || {};
+    if (typeof schema === 'boolean') return undefined;
     return schema[`${key}`];
+  }
+
+  /**
+   * Set the types of the model
+   * 
+   * @param types to set the model type to
+   */
+  setType(types : string | string[]) {
+    if (Array.isArray(types)) {
+      if (types.length === 0) {
+        this.type = undefined;
+        return;
+      } else if (types.length === 1) {
+        this.type = types[0];
+        return;
+      }
+    }
+    this.type = types;
+  }
+
+  /**
+   * Adds types to the existing model types.
+   * 
+   * Makes sure to only keep a single type incase of duplicates.
+   * 
+   * @param types which types we should try and add to the existing output
+   */
+  addToTypes(types: string[] | string) {
+    if (Array.isArray(types)) {
+      types.forEach((value) => {
+        this.addToTypes(value);
+      });
+    } else if (this.type === undefined) {
+      this.type = types;
+    } else if (!Array.isArray(this.type) && this.type !== types) {
+      this.type = [this.type, types];
+    } else if (Array.isArray(this.type) && !this.type.includes(types)) {
+      this.type.push(types);
+    }
   }
 
   /**
@@ -256,5 +114,191 @@ export class CommonModel extends CommonSchema<CommonModel> {
       });
     }
     return dependsOn;
+  }
+
+  /**
+   * Transform object into a type of CommonModel.
+   * 
+   * @param object to transform
+   * @returns CommonModel instance of the object
+   */
+  static toCommonModel(object: unknown): CommonModel {
+    let newCommonModel = new CommonModel();
+    newCommonModel = Object.assign(newCommonModel, object);
+    newCommonModel = CommonSchema.transformSchema(newCommonModel, CommonModel.toCommonModel) as CommonModel;
+    if (newCommonModel.originalSchema !== undefined) {
+      newCommonModel.originalSchema = Schema.toSchema(newCommonModel.originalSchema);
+    }
+    return newCommonModel;
+  }
+  /**
+   * Merge two common model properties together 
+   * 
+   * @param mergeTo CommonModel to merge properties into
+   * @param mergeFrom CommonModel to merge properties from
+   * @param originalSchema schema to use as original schema
+   * @param alreadyIteratedModels to handle circular models correctly
+   */
+  private static mergeProperties(mergeTo: CommonModel, mergeFrom: CommonModel, originalSchema: Schema, alreadyIteratedModels: Map<CommonModel, CommonModel> = new Map()) {
+    const mergeToProperties = mergeTo.properties;
+    const mergeFromProperties = mergeFrom.properties;
+    if (mergeFromProperties !== undefined) {
+      if (mergeToProperties === undefined) {
+        mergeTo.properties = mergeFromProperties;
+      } else {
+        for (const [propName, prop] of Object.entries(mergeFromProperties)) {
+          if (mergeToProperties[`${propName}`] !== undefined) {
+            mergeToProperties[`${propName}`] = CommonModel.mergeCommonModels(mergeToProperties[`${propName}`], prop, originalSchema, alreadyIteratedModels);
+          } else {
+            mergeToProperties[`${propName}`] = prop;
+          }
+        }
+      }
+    }
+  }
+  /**
+   * Merge two common model additional properties together 
+   * 
+   * @param mergeTo CommonModel to merge additional properties into
+   * @param mergeFrom CommonModel to merge additional properties from
+   * @param originalSchema schema to use as original schema
+   * @param alreadyIteratedModels to handle circular models correctly
+   */
+  private static mergeAdditionalProperties(mergeTo: CommonModel, mergeFrom: CommonModel, originalSchema: Schema, alreadyIteratedModels: Map<CommonModel, CommonModel> = new Map()) {
+    const mergeToAdditionalProperties = mergeTo.additionalProperties;
+    const mergeFromAdditionalProperties = mergeFrom.additionalProperties;
+    if (mergeFromAdditionalProperties !== undefined) {
+      if (mergeToAdditionalProperties !== undefined) {
+        mergeTo.additionalProperties = CommonModel.mergeCommonModels(mergeToAdditionalProperties, mergeFromAdditionalProperties, originalSchema, alreadyIteratedModels);
+      } else {
+        mergeTo.additionalProperties = mergeFromAdditionalProperties;
+      }
+    }
+  }
+  /**
+   * Merge two common model pattern properties together 
+   * 
+   * @param mergeTo CommonModel to merge pattern properties into
+   * @param mergeFrom CommonModel to merge pattern properties from
+   * @param originalSchema schema to use as original schema
+   * @param alreadyIteratedModels to handle circular models correctly
+   */
+  private static mergePatternProperties(mergeTo: CommonModel, mergeFrom: CommonModel, originalSchema: Schema, alreadyIteratedModels: Map<CommonModel, CommonModel> = new Map()) {
+    const mergeToPatternProperties = mergeTo.patternProperties;
+    const mergeFromPatternProperties = mergeFrom.patternProperties;
+    if (mergeFromPatternProperties !== undefined) {
+      if (mergeToPatternProperties === undefined) {
+        mergeTo.patternProperties = mergeFromPatternProperties;
+      } else {
+        for (const [pattern, patternModel] of Object.entries(mergeFromPatternProperties)) {
+          if (mergeToPatternProperties[`${pattern}`] !== undefined) {
+            mergeToPatternProperties[`${pattern}`] = CommonModel.mergeCommonModels(mergeToPatternProperties[`${pattern}`], patternModel, originalSchema, alreadyIteratedModels);
+          } else {
+            mergeToPatternProperties[`${pattern}`] = patternModel;
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Merge items together so only one CommonModel remains.
+   * 
+   * @param mergeTo CommonModel to merge items into
+   * @param mergeFrom CommonModel to merge items from
+   * @param originalSchema schema to use as original schema
+   * @param alreadyIteratedModels to handle circular models correctly
+   */
+  private static mergeItems(mergeTo: CommonModel, mergeFrom: CommonModel, originalSchema: Schema, alreadyIteratedModels: Map<CommonModel, CommonModel> = new Map()) {
+    const merge = (models: CommonModel | CommonModel[] | undefined): CommonModel | undefined => {
+      if (Array.isArray(models)) {
+        if (models.length > 0) {
+          let mergedItemsModel: CommonModel | undefined = undefined;
+          models.forEach((model) => { 
+            mergedItemsModel = CommonModel.mergeCommonModels(mergedItemsModel, model, originalSchema, alreadyIteratedModels); 
+          });
+          return mergedItemsModel;
+        } 
+        return undefined;
+      }
+      return models;
+    };
+    if (mergeFrom.items !== undefined) {
+      //Incase of arrays, merge them into a single schema
+      const mergeFromItemsModel = merge(mergeFrom.items);
+      const mergeToItemsModel = merge(mergeTo.items);
+      if (mergeFromItemsModel !== undefined) {
+        if (mergeToItemsModel !== undefined) {
+          mergeTo.items = CommonModel.mergeCommonModels(mergeToItemsModel, mergeFromItemsModel, originalSchema, alreadyIteratedModels);
+        } else {
+          mergeTo.items = mergeFromItemsModel;
+        }
+      }
+    } else if (mergeTo.items !== undefined) {
+      mergeTo.items = merge(mergeTo.items);
+    }
+  }
+
+  /**
+   * Merge types together
+   * 
+   * @param mergeTo CommonModel to merge types into
+   * @param mergeFrom CommonModel to merge from
+   */
+  private static mergeTypes(mergeTo: CommonModel, mergeFrom: CommonModel) {
+    //Only add the types that do not already exist
+    const addToType = (type: string) => {
+      if (mergeTo.type !== undefined && !mergeTo.type.includes(type)) {
+        if (Array.isArray(mergeTo.type)) {
+          mergeTo.type.push(type);
+        } else {
+          mergeTo.type = [mergeTo.type, type];
+        }
+      }
+    };
+    if (mergeFrom.type !== undefined) {
+      if (mergeTo.type === undefined) {
+        mergeTo.type = mergeFrom.type;
+      } else {
+        if (Array.isArray(mergeFrom.type)) {
+          mergeFrom.type.forEach(addToType);
+          return;
+        }
+        addToType(mergeFrom.type);
+      }
+    }
+  }
+
+  /**
+   * Only merge if left side is undefined and right side is sat OR both sides are defined
+   * 
+   * @param mergeTo CommonModel to merge into
+   * @param mergeFrom CommonModel to merge values from
+   * @param originalSchema schema to use as original schema
+   * @param alreadyIteratedModels to handle circular models correctly
+   */
+  static mergeCommonModels(mergeTo: CommonModel | undefined, mergeFrom: CommonModel, originalSchema: Schema, alreadyIteratedModels: Map<CommonModel, CommonModel> = new Map()): CommonModel {
+    if (mergeTo === undefined) return mergeFrom;
+    if (alreadyIteratedModels.has(mergeFrom)) return alreadyIteratedModels.get(mergeFrom) as CommonModel;
+    alreadyIteratedModels.set(mergeFrom, mergeTo);
+
+    CommonModel.mergeAdditionalProperties(mergeTo, mergeFrom, originalSchema, alreadyIteratedModels);
+    CommonModel.mergePatternProperties(mergeTo, mergeFrom, originalSchema, alreadyIteratedModels);
+    CommonModel.mergeProperties(mergeTo, mergeFrom, originalSchema, alreadyIteratedModels);
+    CommonModel.mergeItems(mergeTo, mergeFrom, originalSchema, alreadyIteratedModels);
+    CommonModel.mergeTypes(mergeTo, mergeFrom);
+
+    if (mergeFrom.enum !== undefined) {
+      mergeTo.enum = [... new Set([...(mergeTo.enum || []), ...mergeFrom.enum])];
+    }
+    if (mergeFrom.required !== undefined) {
+      mergeTo.required = [... new Set([...(mergeTo.required || []), ...mergeFrom.required])];
+    }
+    // Which values are correct to use here? Is allOf required?
+    mergeTo.$id = mergeTo.$id || mergeFrom.$id;
+    mergeTo.$ref = mergeTo.$ref || mergeFrom.$ref;
+    mergeTo.extend = mergeTo.extend || mergeFrom.extend;
+    mergeTo.originalSchema = originalSchema;
+    return mergeTo;
   }
 }
