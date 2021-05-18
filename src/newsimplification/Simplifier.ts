cb739e7c56cda115c45eeb2299dbb336df960598
Jonas Lagoni <jonas-lt@live.dk>
2021-05-18 18:37:26 +0200

    Add initial code

--- src/newsimplification/Simplifier.ts
+++ src/newsimplification/Simplifier.ts
@@ -3,6 +3,7 @@ import { SimplificationOptions } from '../models/SimplificationOptions';
 import { simplifyName, isModelObject } from './Utils';
 import simplifyProperties from './SimplifyProperties';
 import { Logger } from '../utils';
+import simplifyPatternProperties from './SimplifyPatternProperties';
 
 export class Simplifier {
   static defaultOptions: SimplificationOptions = {
@@ -80,7 +81,8 @@ export class Simplifier {
       }
 
       simplifyProperties(schema, model, this);
-
+      simplifyPatternProperties(schema, model, this);
+      
       this.combineSchemas(schema.oneOf, model, schema);
       this.combineSchemas(schema.anyOf, model, schema);
       this.combineSchemas(schema.then, model, schema);
