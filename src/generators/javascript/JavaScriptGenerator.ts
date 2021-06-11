129318ca3089b493ebc7c6457d153833e634fbfa
Kanwal Singh <54525960+Blakelist7@users.noreply.github.com>
2021-06-12 03:39:11 +0530

    refactor: replace `switch` statements with `if else` (#245)
    Signed-off-by: Kanwal Singh <kanwal.88singh@gmail.com>

--- src/generators/javascript/JavaScriptGenerator.ts
+++ src/generators/javascript/JavaScriptGenerator.ts
@@ -29,12 +29,10 @@ export class JavaScriptGenerator extends AbstractGenerator<JavaScriptOptions> {
 
   render(model: CommonModel, inputModel: CommonInputModel): Promise<string> {
     const kind = TypeHelpers.extractKind(model);
-    switch (kind) {
-    case ModelKind.OBJECT: {
+    if (kind === ModelKind.OBJECT) {
       return this.renderClass(model, inputModel);
     }
-    default: return Promise.resolve('');
-    }
+    return Promise.resolve('');
   }
 
   renderClass(model: CommonModel, inputModel: CommonInputModel): Promise<string> {
