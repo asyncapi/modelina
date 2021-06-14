da36cb95c8d8593b286d0c57b9f936f350fef77c
Jonas Lagoni <jonas-lt@live.dk>
2021-06-14 18:17:46 +0200

    removed unnecessary eslint

--- src/generators/java/presets/CommonPreset.ts
+++ src/generators/java/presets/CommonPreset.ts
@@ -91,7 +91,6 @@ private String toIndentedString(Object o) {
  * 
  * @implements {JavaPreset}
  */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 export const JAVA_COMMON_PRESET: JavaPreset = {
   class: {
     additionalContent({ renderer, model, content, options }) {
