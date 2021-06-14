31738fca4eaa0275c18b28a510cf28e1044e4d57
Jonas Lagoni <jonas-lt@live.dk>
2021-06-14 16:27:48 +0200

    Adds interpretation of dependencies

---
+++ src/interpreter/InterpretDependencies.ts
@@ -0,0 +1,20 @@
+
+import { CommonModel } from '../models/CommonModel';
+import { Schema } from '../models/Schema';
+import { Interpreter, InterpreterOptions } from './Interpreter';
+
+/**
+ * Interpreter function for JSON Schema draft 7 dependencies keyword.
+ * 
+ * @param schema 
+ * @param model
+ */
+export default function interpretDependencies(schema: Schema, model: CommonModel, interpreter: Interpreter, interpreterOptions: InterpreterOptions = Interpreter.defaultInterpreterOptions): void {
+  if (schema.dependencies === undefined) {return;}
+  for (const dependency of Object.values(schema.dependencies)) {
+    if (!Array.isArray(dependency)) {
+      //Schema dependency
+      interpreter.interpretAndCombineSchema(dependency, model, schema, interpreterOptions);
+    }
+  }
+}
