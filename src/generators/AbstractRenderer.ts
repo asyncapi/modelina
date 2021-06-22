37b960916e29fb49238a1f59e8c87d81a3f89ad8
Jonas Lagoni <jonas-lt@live.dk>
2021-06-22 14:07:50 +0200

    Adds basic JSDoc

--- src/generators/AbstractRenderer.ts
+++ src/generators/AbstractRenderer.ts
@@ -38,18 +38,30 @@ export abstract class AbstractRenderer<
     return lines.filter(Boolean).join(n);
   }
 
+  /**
+   * Renders the name of a type based on provided generator option naming convention type callback.
+   * 
+   * This is used to render names of models (example TS class) and then later used if that class is referenced from other models.
+   * 
+   * @param name 
+   * @param model 
+   */
   nameType(name: string | undefined, model?: CommonModel): string {
-    name = name || '';
     return this.options?.namingConvention?.type 
       ? this.options.namingConvention.type(name, { model: model || this.model, inputModel: this.inputModel })
-      : name;
+      : name || '';
   }
 
-  nameProperty(name: string | undefined, property?: CommonModel): string {
-    name = name || '';
+  /**
+   * Renders the name of a property based on provided generator option naming convention property callback.
+   * 
+   * @param propertyName 
+   * @param property
+   */
+  nameProperty(propertyName: string | undefined, property?: CommonModel): string {
     return this.options?.namingConvention?.property 
-      ? this.options.namingConvention.property(name, { model: this.model, inputModel: this.inputModel, property })
-      : name;
+      ? this.options.namingConvention.property(propertyName, { model: this.model, inputModel: this.inputModel, property })
+      : propertyName || '';
   }
 
   indent(
