1dfc2053311a69917fa83c09f3c0753a8e866eee
Jonas Lagoni <jonas-lt@live.dk>
2021-08-16 16:47:41 +0200

    Renamed argument

--- src/generators/AbstractGenerator.ts
+++ src/generators/AbstractGenerator.ts
@@ -10,7 +10,7 @@ export interface CommonGeneratorOptions<P extends Preset = Preset> {
   };
   defaultPreset?: P;
   presets?: Presets<P>;
-  parserOptions?: ProcessOptions
+  processOptions?: ProcessOptions
 }
 
 export const defaultGeneratorOptions: CommonGeneratorOptions = {
@@ -37,7 +37,7 @@ export abstract class AbstractGenerator<Options extends CommonGeneratorOptions =
   public abstract render(model: CommonModel, inputModel: CommonInputModel): Promise<RenderOutput>;
 
   public async process(input: Record<string, unknown>): Promise<CommonInputModel> {
-    return await InputProcessor.processor.process(input, this.options.parserOptions);
+    return await InputProcessor.processor.process(input, this.options.processOptions);
   }
 
   public async generate(input: Record<string, unknown> | CommonInputModel): Promise<OutputModel[]> {
