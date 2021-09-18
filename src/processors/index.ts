c1e3b12a3d49d2a254ef82804531913d18fca352
Jonas Lagoni <jonas-lt@live.dk>
2021-09-18 14:14:07 +0200

    Removed unused processor

--- src/processors/index.ts
+++ src/processors/index.ts
@@ -2,4 +2,3 @@ export * from './AbstractInputProcessor';
 export * from './InputProcessor';
 export * from './AsyncAPIInputProcessor';
 export * from './JsonSchemaInputProcessor';
-export * from './SwaggerInputProcessor';
