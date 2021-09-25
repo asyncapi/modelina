7a94c92b8cb29571b2e3c76fa96889d84d761173
Jonas Lagoni <jonas-lt@live.dk>
2021-09-25 16:04:34 +0200

    Fixing tests for windows

--- test/helpers/FileHelpers.spec.ts
+++ test/helpers/FileHelpers.spec.ts
@@ -1,5 +1,6 @@
 import {promises as fs} from 'fs';
 import { FileHelpers } from '../../src/helpers';
+import * as path from 'path';
 describe('FileHelpers', () => {
   afterEach(() => {
     jest.restoreAllMocks();
@@ -8,16 +9,28 @@ describe('FileHelpers', () => {
     test('should write file content to correct location', async () => { 
       jest.spyOn(fs, 'mkdir').mockResolvedValue(undefined);
       jest.spyOn(fs, 'writeFile').mockResolvedValue(undefined);
-      await FileHelpers.writeToFile('content', '/test/output.ts');
-      expect(fs.mkdir).toHaveBeenNthCalledWith(1, '/test', { recursive: true });
-      expect(fs.writeFile).toHaveBeenNthCalledWith(1, '/test/output.ts', 'content');
+
+      const outputDir = './test';
+      const outputFile = `${outputDir}/output.ts`;
+      const expectedOutputDirPath = path.resolve(outputDir);
+      const expectedOutputFilePath = path.resolve(outputFile);
+
+      await FileHelpers.writeToFile('content', outputFile);
+      
+      expect(fs.mkdir).toHaveBeenNthCalledWith(1, expectedOutputDirPath, { recursive: true });
+      expect(fs.writeFile).toHaveBeenNthCalledWith(1, expectedOutputFilePath, 'content');
     });
     test('should handle rejected promises', async () => { 
       const error = new Error('Test error');
       jest.spyOn(fs, 'mkdir').mockRejectedValue(error);
       jest.spyOn(fs, 'writeFile').mockResolvedValue(undefined);
-      await expect(FileHelpers.writeToFile('content', '/test/output.ts')).rejects.toEqual(error);
-      expect(fs.mkdir).toHaveBeenNthCalledWith(1, '/test', { recursive: true });
+
+      const outputDir = './test';
+      const outputFile = `${outputDir}/output.ts`;
+      const expectedOutputPath = path.resolve(outputDir);
+
+      await expect(FileHelpers.writeToFile('content', outputFile)).rejects.toEqual(error);
+      expect(fs.mkdir).toHaveBeenNthCalledWith(1, expectedOutputPath, { recursive: true });
       expect(fs.writeFile).not.toHaveBeenCalled();
     });
   });
