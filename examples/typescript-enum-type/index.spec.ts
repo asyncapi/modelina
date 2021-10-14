49778a7e36fd56d5a4434f3934ce94a0f99551b3
Oskar <oskar.baumann@tum.de>
2021-10-14 19:37:36 +0200

    Fix linting and add to example Readme

--- examples/typescript-enum-type/index.spec.ts
+++ examples/typescript-enum-type/index.spec.ts
@@ -7,7 +7,7 @@ describe('Should be able to render correct enums based on options', () => {
   afterAll(() => {
     jest.restoreAllMocks();
   });
-  test('and should log expected output to console', async function testIt(): Promise<void>{
+  test('and should log expected output to console', async function testIt(): Promise<void> {
     // Wait for first call to the `generate` function is over, and we can reliably know the output
     if (spy.mock.calls.length !== 6) {
       await timeout(50);
@@ -18,7 +18,7 @@ describe('Should be able to render correct enums based on options', () => {
     //Generate is called 2 times, so even though we expect 6 console logs we double it
     expect(spy.mock.calls.length).toEqual(12);
     const generatedContent = spy.mock.calls.slice(6).join('\n');
-    expect(generatedContent).toMatchSnapshot(); 
+    expect(generatedContent).toMatchSnapshot();
   });
 });
 
