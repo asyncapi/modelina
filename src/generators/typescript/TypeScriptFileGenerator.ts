50292bed8ff275788d6b9f9437bc7929d4886081
Jonas Lagoni <jonas-lt@live.dk>
2021-12-21 16:21:28 +0100

    Added file generator.

---
+++ src/generators/typescript/TypeScriptFileGenerator.ts
@@ -0,0 +1,24 @@
+import { TypeScriptGenerator, TypeScriptRenderCompleteModelOptions } from './';
+import { CommonInputModel, OutputModel } from '../../models';
+import * as path from 'path';
+import { AbstractFileGenerator } from '../AbstractFileGenerator';
+import { FileHelpers } from '../../helpers';
+
+export class TypeScriptFileGenerator extends TypeScriptGenerator implements AbstractFileGenerator<TypeScriptRenderCompleteModelOptions> {
+  /**
+   * Generates all the models to an output directory each model with their own separate files. 
+   * 
+   * @param input
+   * @param outputDirectory where you want the models generated to
+   * @param options
+   */
+  public async generateToFiles(input: Record<string, unknown> | CommonInputModel, outputDirectory: string, options?: TypeScriptRenderCompleteModelOptions): Promise<OutputModel[]> {
+    let generatedModels = await this.generateCompleteModels(input, options || {});
+    generatedModels = generatedModels.filter((outputModel) => { return outputModel.modelName !== undefined; });
+    for (const outputModel of generatedModels) {
+      const filePath = path.resolve(outputDirectory, `${outputModel.modelName}.ts`);
+      await FileHelpers.writerToFileSystem(outputModel.result, filePath);
+    }
+    return generatedModels;
+  }
+}
---
+++ test/generators/typescript/TypeScriptFileGenerator.spec.ts
@@ -0,0 +1,51 @@
+import { CommonInputModel, CommonModel, FileHelpers, TypeScriptFileGenerator, OutputModel } from '../../../src';
+import * as path from 'path';
+
+describe('TypeScriptFileGenerator', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  describe('generateToFile()', () => {
+    const doc = {
+      $id: 'CustomClass',
+      type: 'object',
+      additionalProperties: true,
+      properties: {
+        someProp: { type: 'string' },
+        someEnum: {
+          $id: 'CustomEnum',
+          type: 'string',
+          enum: ['Texas', 'Alabama', 'California'],
+        }
+      }
+    };
+    test('should throw accurate error if file cannot be written', async () => {
+      const generator = new TypeScriptFileGenerator();
+      const expectedError = new Error('write error');
+      jest.spyOn(FileHelpers, 'writerToFileSystem').mockRejectedValue(expectedError);
+      jest.spyOn(generator, 'generateCompleteModels').mockResolvedValue([new OutputModel('content', new CommonModel(), '', new CommonInputModel(), [])]);
+    
+      await expect(generator.generateToFiles(doc, '/test/')).rejects.toEqual(expectedError);
+      expect(generator.generateCompleteModels).toHaveBeenCalledTimes(1);
+      expect(FileHelpers.writerToFileSystem).toHaveBeenCalledTimes(1);
+    });
+    test('should try and generate models to files', async () => {
+      const generator = new TypeScriptFileGenerator();
+      const outputDir = './test';
+      const expectedOutputDirPath = path.resolve(outputDir);
+      const expectedOutputFilePath = path.resolve(`${outputDir}/Test.ts`);
+      const expectedWriteToFileParameters = [
+        'content',
+        expectedOutputFilePath,
+      ];
+      jest.spyOn(FileHelpers, 'writerToFileSystem').mockResolvedValue(undefined);
+      jest.spyOn(generator, 'generateCompleteModels').mockResolvedValue([new OutputModel('content', new CommonModel(), 'Test', new CommonInputModel(), [])]);
+      
+      await generator.generateToFiles(doc, expectedOutputDirPath, {packageName: 'SomePackage'});
+      expect(generator.generateCompleteModels).toHaveBeenCalledTimes(1);
+      expect(FileHelpers.writerToFileSystem).toHaveBeenCalledTimes(1);
+      expect((FileHelpers.writerToFileSystem as jest.Mock).mock.calls[0]).toEqual(expectedWriteToFileParameters);
+    });
+  });
+});
