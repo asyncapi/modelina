8cb97c1f46a8d67bf07872f6e3de83068b93e731
Samridhi-98 <agrawalsamriddhi83@gmail.com>
2022-01-17 20:07:21 +0530

    Added test for un/marshalling in JS

--- src/generators/javascript/presets/CommonPreset.ts
+++ src/generators/javascript/presets/CommonPreset.ts
@@ -2,11 +2,9 @@ import { JavaScriptRenderer } from '../JavaScriptRenderer';
 import { JavaScriptPreset } from '../JavaScriptPreset';
 import { getUniquePropertyName, DefaultPropertyNames, TypeHelpers, ModelKind } from '../../../helpers';
 import { CommonInputModel, CommonModel } from '../../../models';
-import renderExampleFunction from './utils/ExampleFunction';
 
 export interface JavaScriptCommonPresetOptions {
   marshalling: boolean;
-  example: boolean;
 }
 
 function realizePropertyFactory(prop: string) {
@@ -148,7 +146,7 @@ function renderUnmarshalAdditionalProperties(model: CommonModel, renderer: JavaS
   if (model.additionalProperties !== undefined) {
     let additionalPropertyName = getUniquePropertyName(model, DefaultPropertyNames.additionalProperties);
     additionalPropertyName = renderer.nameProperty(additionalPropertyName, model.additionalProperties);
-    const modelInstanceVariable = 'value as any';
+    const modelInstanceVariable = 'value';
     const unmarshalCode = renderUnmarshalProperty(modelInstanceVariable, model.additionalProperties, inputModel, renderer);
     setAdditionalPropertiesMap = `if (instance.${additionalPropertyName} === undefined) {instance.${additionalPropertyName} = new Map();}`;
     unmarshalAdditionalProperties = `instance.${additionalPropertyName}.set(key, ${unmarshalCode});`;
@@ -172,7 +170,7 @@ function renderUnmarshal({ renderer, model, inputModel }: {
   const propertyNames = Object.keys(properties).map((prop => `"${prop}"`));
   return `public static unmarshal(json: string | object): ${formattedModelName} {
   const obj = typeof json === "object" ? json : JSON.parse(json);
-  const instance = new ${formattedModelName}({} as any);
+  const instance = new ${formattedModelName}({});
 
 ${renderer.indent(unmarshalProperties)}
 
@@ -188,7 +186,7 @@ ${renderer.indent(unmarshalAdditionalProperties, 4)}
 }
 
 /**
- * Preset which adds `marshal`, `unmarshal`, `example` functions to class. 
+ * Preset which adds `marshal`, `unmarshal` functions to class. 
  * 
  * @implements {JavaScriptPreset}
  */
@@ -201,10 +199,6 @@ export const JS_COMMON_PRESET: JavaScriptPreset = {
       if (options.marshalling === true) {
         blocks.push(renderMarshal({ renderer, model, inputModel }));
         blocks.push(renderUnmarshal({ renderer, model, inputModel }));
-      }
-
-      if (options.example === true) {
-        blocks.push(renderExampleFunction({ renderer, model }));
       }
 
       return renderer.renderBlock([content, ...blocks], 2);
