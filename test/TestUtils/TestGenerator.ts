b50ed836d48d48c71b0952192ec311f0ff55073f
Jonas Lagoni <jonas-lt@live.dk>
2022-04-25 23:30:23 +0200

    fix linting

--- test/TestUtils/TestGenerator.ts
+++ test/TestUtils/TestGenerator.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { AbstractGenerator, CommonInputModel, CommonModel, IndentationTypes, RenderOutput } from "../../src";
+import { AbstractGenerator, CommonInputModel, CommonModel, IndentationTypes, RenderOutput } from '../../src';
 
 export const testOptions = {
   indentation: {
