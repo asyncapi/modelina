4cbe2aed323a23b970bb3ce7e8edf7af41f4aab2
Leigh Johnson <leigh@bitsy.ai>
2022-07-28 19:43:03 -0700

    chore: add Rust to blackbox test spec

--- test/blackbox/blackbox.spec.ts
+++ test/blackbox/blackbox.spec.ts
@@ -7,8 +7,8 @@
 
 import * as path from 'path';
 import * as fs from 'fs';
-import { GoFileGenerator, CSharpFileGenerator, JavaFileGenerator, JAVA_COMMON_PRESET, TypeScriptFileGenerator, JavaScriptFileGenerator } from '../../src';
-import { execCommand, generateModels, renderModels, renderModelsToSeparateFiles } from './utils/Utils';
+import { RustFileGenerator, defaultRustRenderCompleteModelOptions, RustPackageFeatures, RustRenderCompleteModelOptions, GoFileGenerator, CSharpFileGenerator, JavaFileGenerator, JAVA_COMMON_PRESET, TypeScriptFileGenerator, JavaScriptFileGenerator } from '../../src';
+import { execCommand } from './utils/Utils';
 
 /**
  * Read all the files in the folder, and return the appropriate Jest `each` entries.
@@ -17,8 +17,8 @@ import { execCommand, generateModels, renderModels, renderModelsToSeparateFiles 
 function readFilesInFolder(folder: string) {
   const fullPath = path.resolve(__dirname, `./docs/${folder}`);
   return fs.readdirSync(fullPath).map(
-    (file) => { 
-      return { file: `./docs/${folder}/${file}`, outputDirectory: `./output/${folder}/${path.parse(file).name}`};
+    (file) => {
+      return { file: `./docs/${folder}/${file}`, outputDirectory: `./output/${folder}/${path.parse(file).name}` };
     }
   );
 }
@@ -33,6 +33,7 @@ const AsyncAPIV2_3Files = readFilesInFolder('AsyncAPI-2_3');
 const AsyncAPIV2_4Files = readFilesInFolder('AsyncAPI-2_4');
 
 const filesToTest = [
+<<<<<<< HEAD
   // ...OpenAPI3_0Files,
   // ...AsyncAPIV2_0Files,
   // ...AsyncAPIV2_1Files,
@@ -51,27 +52,95 @@ const filesToTest = [
   }),
   // ...jsonSchemaDraft7Files,
   // ...jsonSchemaDraft6Files,
+=======
+  ...OpenAPI3_0Files.filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/456
+    return file !== './docs/OpenAPI-3_0/twilio-1_13.json';
+  }).filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/452
+    return file !== './docs/OpenAPI-3_0/postman-api.json';
+  }),
+  ...AsyncAPIV2_0Files.filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/504
+    return file !== './docs/AsyncAPI-2_0/dummy.json';
+  }),
+  ...AsyncAPIV2_1Files.filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/504
+    return file !== './docs/AsyncAPI-2_1/dummy.json';
+  }),
+  ...AsyncAPIV2_2Files.filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/504
+    return file !== './docs/AsyncAPI-2_2/dummy.json';
+  }),
+  ...AsyncAPIV2_3Files.filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/504
+    return file !== './docs/AsyncAPI-2_3/dummy.json';
+  }),
+  ...AsyncAPIV2_4Files.filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/504
+    return file !== './docs/AsyncAPI-2_4/dummy.json';
+  }),
+  ...jsonSchemaDraft7Files.filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/388
+    return file !== './docs/JsonSchemaDraft-7/draft-7-core.json';
+  }).filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/390
+    return file !== './docs/JsonSchemaDraft-7/graphql-code-generator.json';
+  }),
+  ...jsonSchemaDraft4Files.filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/449
+    return file !== './docs/JsonSchemaDraft-4/openapi-3.json';
+  }).filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/389
+    return file !== './docs/JsonSchemaDraft-4/jenkins-config.json';
+  }).filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/450
+    return file !== './docs/JsonSchemaDraft-4/circleci-config.json';
+  }).filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/390
+    return file !== './docs/JsonSchemaDraft-4/circleci-config.json';
+  }).filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/452
+    return file !== './docs/JsonSchemaDraft-4/chrome-manifest.json';
+  }).filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/367
+    return file !== './docs/JsonSchemaDraft-4/aws-cloudformation.json';
+  }).filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/388
+    return file !== './docs/JsonSchemaDraft-4/draft-4-core.json';
+  }),
+  ...jsonSchemaDraft6Files.filter(({ file }) => {
+    //Blocked by https://github.com/asyncapi/modelina/issues/453
+    return file !== './docs/JsonSchemaDraft-6/fhir-full.json';
+  })
+>>>>>>> f77edfc (chore: add Rust to blackbox test spec)
 ];
 
 // eslint-disable-next-line no-console
 console.log('This is gonna take some time, Stay Awhile and Listen');
-describe.each(filesToTest)('Should be able to generate with inputs', ({file, outputDirectory}) => {
+describe.each(filesToTest)('Should be able to generate with inputs', ({ file, outputDirectory }) => {
   jest.setTimeout(1000000);
   const fileToGenerateFor = path.resolve(__dirname, file);
   const outputDirectoryPath = path.resolve(__dirname, outputDirectory);
   beforeAll(async () => {
     if (fs.existsSync(outputDirectoryPath)) {
-      await fs.rmSync(outputDirectoryPath, {recursive: true});
+      await fs.rmSync(outputDirectoryPath, { recursive: true });
     }
   });
   describe(file, () => {
     const javaGeneratorOptions = [
+<<<<<<< HEAD
       { 
         generatorOption: { },
         description: 'default generator',
         renderOutputPath: path.resolve(outputDirectoryPath, './java/class/default')
+=======
+      {
+        generatorOption: {},
+        description: 'default generator'
+>>>>>>> f77edfc (chore: add Rust to blackbox test spec)
       },
-      { 
+      {
         generatorOption: {
           presets: [
             JAVA_COMMON_PRESET
@@ -80,15 +149,20 @@ describe.each(filesToTest)('Should be able to generate with inputs', ({file, out
         description: 'all common presets',
         renderOutputPath: path.resolve(outputDirectoryPath, './java/class/commonpreset')
       }
+<<<<<<< HEAD
     ]; 
     describe.each(javaGeneratorOptions)('should be able to generate and compile Java', ({generatorOption, description, renderOutputPath}) => {
+=======
+    ];
+    describe.each(javaGeneratorOptions)('should be able to generate and compile Java', ({ generatorOption, description }) => {
+>>>>>>> f77edfc (chore: add Rust to blackbox test spec)
       test('class', async () => {
         const generator = new JavaFileGenerator(generatorOption);
         const inputFileContent = await fs.promises.readFile(fileToGenerateFor);
         const input = JSON.parse(String(inputFileContent));
         const dependencyPath = path.resolve(__dirname, './dependencies/java/*');
 
-        const generatedModels = await generator.generateToFiles(input, renderOutputPath, {packageName: 'TestPackageName'});
+        const generatedModels = await generator.generateToFiles(input, renderOutputPath, { packageName: 'TestPackageName' });
         expect(generatedModels).not.toHaveLength(0);
 
         const compileCommand = `javac  -cp ${dependencyPath} ${path.resolve(renderOutputPath, '*.java')}`;
@@ -101,10 +175,10 @@ describe.each(filesToTest)('Should be able to generate with inputs', ({file, out
         const inputFileContent = await fs.promises.readFile(fileToGenerateFor);
         const input = JSON.parse(String(inputFileContent));
         const renderOutputPath = path.resolve(outputDirectoryPath, './csharp');
-        
-        const generatedModels = await generator.generateToFiles(input, renderOutputPath, {namespace: 'TestNamespace'});
+
+        const generatedModels = await generator.generateToFiles(input, renderOutputPath, { namespace: 'TestNamespace' });
         expect(generatedModels).not.toHaveLength(0);
-        
+
         const compileCommand = `csc /target:library /out:${path.resolve(renderOutputPath, './compiled.dll')} ${path.resolve(renderOutputPath, '*.cs')}`;
         await execCommand(compileCommand);
       });
@@ -112,7 +186,7 @@ describe.each(filesToTest)('Should be able to generate with inputs', ({file, out
 
     describe('should be able to generate and transpile TS', () => {
       test('class', async () => {
-        const generator = new TypeScriptFileGenerator({modelType: 'class'});
+        const generator = new TypeScriptFileGenerator({ modelType: 'class' });
         const inputFileContent = await fs.promises.readFile(fileToGenerateFor);
         const input = JSON.parse(String(inputFileContent));
         const renderOutputPath = path.resolve(outputDirectoryPath, './ts/class');
@@ -121,11 +195,11 @@ describe.each(filesToTest)('Should be able to generate with inputs', ({file, out
         expect(generatedModels).not.toHaveLength(0);
 
         const transpileCommand = `tsc --downlevelIteration -t es5 --baseUrl ${renderOutputPath}`;
-        await execCommand(transpileCommand); 
+        await execCommand(transpileCommand);
       });
 
       test('interface', async () => {
-        const generator = new TypeScriptFileGenerator({modelType: 'interface'});
+        const generator = new TypeScriptFileGenerator({ modelType: 'interface' });
         const inputFileContent = await fs.promises.readFile(fileToGenerateFor);
         const input = JSON.parse(String(inputFileContent));
         const renderOutputPath = path.resolve(outputDirectoryPath, './ts/interface');
@@ -145,13 +219,13 @@ describe.each(filesToTest)('Should be able to generate with inputs', ({file, out
         const input = JSON.parse(String(inputFileContent));
         const renderOutputPath = path.resolve(outputDirectoryPath, './js/class');
 
-        const generatedModels = await generator.generateToFiles(input, renderOutputPath, {moduleSystem: 'CJS'});
+        const generatedModels = await generator.generateToFiles(input, renderOutputPath, { moduleSystem: 'CJS' });
         expect(generatedModels).not.toHaveLength(0);
 
         const files = fs.readdirSync(renderOutputPath);
         for (const file of files) {
           const transpileAndRunCommand = `node --check ${path.resolve(renderOutputPath, file)}`;
-          await execCommand(transpileAndRunCommand); 
+          await execCommand(transpileAndRunCommand);
         }
       });
     });
@@ -163,10 +237,41 @@ describe.each(filesToTest)('Should be able to generate with inputs', ({file, out
         const input = JSON.parse(String(inputFileContent));
         const renderOutputPath = path.resolve(outputDirectoryPath, './go/struct/');
 
-        const generatedModels = await generator.generateToFiles(input, renderOutputPath, {packageName: 'test_package_name'});
+        const generatedModels = await generator.generateToFiles(input, renderOutputPath, { packageName: 'test_package_name' });
         expect(generatedModels).not.toHaveLength(0);
 
         const compileCommand = `gofmt ${renderOutputPath}`;
+        await execCommand(compileCommand);
+      });
+    });
+    describe('should be able to generate Rust', () => {
+      test('struct with serde_json', async () => {
+        const generator = new RustFileGenerator();
+        const inputFileContent = await fs.promises.readFile(fileToGenerateFor);
+        const input = JSON.parse(String(inputFileContent));
+        const renderOutputPath = path.resolve(outputDirectoryPath, './rust/struct/');
+        const options = {
+          ...defaultRustRenderCompleteModelOptions,
+          supportFiles: true, // generate Cargo.toml and lib.rs
+          package: {
+            packageName: 'asyncapi-rs-example',
+            packageVersion: '1.0.0',
+            // set authors, homepage, repository, and license
+            authors: ['AsyncAPI Rust Champions'],
+            homepage: 'https://www.asyncapi.com/tools/modelina',
+            repository: 'https://github.com/asyncapi/modelina',
+            license: 'Apache-2.0',
+            description: 'Rust models generated by AsyncAPI Modelina',
+            // support 2018 editions and up
+            edition: '2018',
+            // enable serde_json
+            packageFeatures: [RustPackageFeatures.json] as RustPackageFeatures[]
+          }
+        } as RustRenderCompleteModelOptions
+        const generatedModels = await generator.generateToFiles(input, renderOutputPath, options);
+        expect(generatedModels).not.toHaveLength(0);
+
+        const compileCommand = `cargo build ${renderOutputPath}`;
         await execCommand(compileCommand);
       });
     });
