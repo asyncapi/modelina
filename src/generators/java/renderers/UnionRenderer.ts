1faebef99aa89bc2bd1166683b67c7af1d3f9b5e
Kenneth Aasan <k.aasan@sportradar.com>
2022-10-20 11:02:47 +0200

    feat: support oneOf/union for Java

--- src/generators/java/renderers/UnionRenderer.ts
+++ src/generators/java/renderers/UnionRenderer.ts
@@ -11,11 +11,30 @@ import { JavaRenderer } from '../JavaRenderer';
 export class UnionRenderer extends JavaRenderer<ConstrainedUnionModel> {
   public defaultSelf(): string {
     const doc = this.renderComments(`${this.model.name} represents a union of types: ${this.model.union.map(m => m.type).join(', ')}`);
+
+    const content = [
+      this.renderEnum(),
+    ];
+
     return `${doc}
 public class ${this.model.name} {
-${this.indent(this.model.type)}
+${this.indent(this.renderBlock(content, 2))}
 }
 `;
+  }
+
+  renderEnum(): string {
+    const content: string[] = [];
+
+    const unionTypes = this.model.union.map((unionModel) => {
+      return unionModel.type;
+    }).join(', ');
+
+    content.push(`private enum ${this.model.name}Case {
+  ${unionTypes}
+}`);
+
+    return this.renderBlock(content);
   }
 }
 
