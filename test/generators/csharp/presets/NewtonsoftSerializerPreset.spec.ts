7f44d3228863fc939c41ec729cbb8d33e21144e6
jonaslagoni <jonas-lt@live.dk>
2022-10-24 00:08:35 +0200

    add test

---
+++ test/generators/csharp/presets/NewtonsoftSerializerPreset.spec.ts
@@ -0,0 +1,33 @@
+import { CSharpGenerator, CSHARP_NEWTONSOFT_SERIALIZER_PRESET } from '../../../../src/generators'; 
+const doc = {
+  $id: 'Test',
+  type: 'object',
+  additionalProperties: true,
+  required: ['string prop'],
+  properties: {
+    'string prop': { type: 'string' },
+    numberProp: { type: 'number' },
+    enumProp: { $id: 'EnumTest', enum: ['Some enum String', true, {test: 'test'}, 2]},
+    objectProp: { type: 'object', $id: 'NestedTest', properties: {stringProp: { type: 'string' }}}
+  },
+  patternProperties: {
+    '^S(.?)test': {
+      type: 'string'
+    }
+  },
+};
+describe('Newtonsoft JSON serializer preset', () => {
+  test('should render serialize and deserialize converters', async () => {
+    const generator = new CSharpGenerator({ 
+      presets: [
+        CSHARP_NEWTONSOFT_SERIALIZER_PRESET
+      ]
+    });
+    
+    const inputModel = await generator.generate(doc);
+    expect(inputModel).toHaveLength(3);
+    expect(inputModel[0].result).toMatchSnapshot();
+    expect(inputModel[1].result).toMatchSnapshot();
+    expect(inputModel[2].result).toMatchSnapshot();
+  });
+});
