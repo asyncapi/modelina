48dfc73c4540b251a7fe3e248ef2be2367cf6e6f
jonaslagoni <jonas-lt@live.dk>
2022-10-24 19:53:52 +0200

    update snapshots

--- src/generators/csharp/presets/NewtonsoftSerializerPreset.ts
+++ src/generators/csharp/presets/NewtonsoftSerializerPreset.ts
@@ -9,13 +9,13 @@ function renderSerialize({ model }: {
   model: ConstrainedObjectModel
 }): string {
   const corePropsWrite = Object.values(model.properties)
-  .filter((prop) => !(prop.property instanceof ConstrainedDictionaryModel) || prop.property.serializationType === 'normal')
-  .map((prop) => {
-    return `if (value.${prop.propertyName} != null)
+    .filter((prop) => !(prop.property instanceof ConstrainedDictionaryModel) || prop.property.serializationType === 'normal')
+    .map((prop) => {
+      return `if (value.${prop.propertyName} != null)
 {
-jo.Add("${prop.unconstrainedPropertyName}", JToken.FromObject(value.${prop.propertyName}, serializer));
+  jo.Add("${prop.unconstrainedPropertyName}", JToken.FromObject(value.${prop.propertyName}, serializer));
 }`;
-  });
+    });
   const unwrapPropsWrite = Object.values(model.properties)
     .filter((prop) => prop.property instanceof ConstrainedDictionaryModel && prop.property.serializationType === 'unwrap')
     .map((prop) => {
@@ -23,9 +23,9 @@ jo.Add("${prop.unconstrainedPropertyName}", JToken.FromObject(value.${prop.prope
   {
   foreach (var unwrapProperty in value.${prop.propertyName})
   {
-  var hasProp = jo[unwrapProperty.Key]; 
-  if (hasProp != null) continue;
-  jo.Add(unwrapProperty.Key, JToken.FromObject(unwrapProperty.Value, serializer));
+    var hasProp = jo[unwrapProperty.Key]; 
+    if (hasProp != null) continue;
+    jo.Add(unwrapProperty.Key, JToken.FromObject(unwrapProperty.Value, serializer));
   }
 }`;
     });
@@ -38,7 +38,7 @@ jo.Add("${prop.unconstrainedPropertyName}", JToken.FromObject(value.${prop.prope
 
   jo.WriteTo(writer);
 }`;
-} 
+}
 
 
 /**
@@ -47,31 +47,33 @@ jo.Add("${prop.unconstrainedPropertyName}", JToken.FromObject(value.${prop.prope
 function renderDeserialize({ model }: {
   model: ConstrainedObjectModel
 }): string {
+  const unwrapDictionaryProps = Object.values(model.properties)
+    .filter((prop) => prop.property instanceof ConstrainedDictionaryModel && prop.property.serializationType === 'unwrap');
   const corePropsRead = Object.values(model.properties)
-  .filter((prop) => !(prop.property instanceof ConstrainedDictionaryModel) || prop.property.serializationType === 'normal')
-  .map((prop) => {
-    return `value.${prop.propertyName} = jo["${prop.unconstrainedPropertyName}"].ToObject<${prop.property.type}>(serializer);`;
-  });
-const nonDictionaryPropCheck = Object.values(model.properties)
-  .filter((prop) => prop.property instanceof ConstrainedDictionaryModel && prop.property.serializationType === 'unwrap')
-  .map((prop) => {
-    return `prop.Name != "${prop.unconstrainedPropertyName}"`;
-  });
-const dictionaryPropCheck = Object.values(model.properties)
-  .filter((prop) => prop.property instanceof ConstrainedDictionaryModel && prop.property.serializationType === 'unwrap')
-  .map((prop) => {
-    return `prop.Name == "${prop.unconstrainedPropertyName}"`;
-  });
-const dictionaryInitializers = Object.values(model.properties)
-  .filter((prop) => prop.property instanceof ConstrainedDictionaryModel && prop.property.serializationType === 'unwrap')
-  .map((prop) => {
-    return `value.${prop.propertyName} = new Dictionary<${(prop.property as ConstrainedDictionaryModel).key.type}, ${(prop.property as ConstrainedDictionaryModel).value.type}>();`;
-  });
-const unwrapDictionaryRead = Object.values(model.properties)
-  .filter((prop) => prop.property instanceof ConstrainedDictionaryModel && prop.property.serializationType === 'unwrap')
-  .map((prop) => {
-    return `value.${prop.propertyName}[additionalProperty.Name] = JsonConvert.DeserializeObject(additionalProperty.Value.ToString());`;
-  });
+    .filter((prop) => !(prop.property instanceof ConstrainedDictionaryModel) || prop.property.serializationType === 'normal')
+    .map((prop) => {
+      return `value.${prop.propertyName} = jo["${prop.unconstrainedPropertyName}"].ToObject<${prop.property.type}>(serializer);`;
+    });
+  const nonDictionaryPropCheck = unwrapDictionaryProps.map((prop) => {
+      return `prop.Name != "${prop.unconstrainedPropertyName}"`;
+    });
+  const dictionaryPropCheck = unwrapDictionaryProps.map((prop) => {
+      return `prop.Name == "${prop.unconstrainedPropertyName}"`;
+    });
+  const dictionaryInitializers = unwrapDictionaryProps.map((prop) => {
+      return `value.${prop.propertyName} = new Dictionary<${(prop.property as ConstrainedDictionaryModel).key.type}, ${(prop.property as ConstrainedDictionaryModel).value.type}>();`;
+    });
+  const unwrapDictionaryRead = unwrapDictionaryProps.map((prop) => {
+      return `value.${prop.propertyName}[additionalProperty.Name] = JsonConvert.DeserializeObject(additionalProperty.Value.ToString());`;
+    });
+  const additionalPropertiesCode = unwrapDictionaryProps.length !== 0 ? `var additionalProperties = jo.Properties().Where((prop) => ${nonDictionaryPropCheck.join(' || ')});
+  var coreProperties = jo.Properties().Where((prop) => ${dictionaryPropCheck.join(' || ')});
+  ${dictionaryInitializers}
+
+  foreach (var additionalProperty in additionalProperties)
+  {
+    ${unwrapDictionaryRead.join('\n')}
+  }` : '';
   return `public override ${model.name} ReadJson(JsonReader reader, Type objectType, ${model.name} existingValue, bool hasExistingValue, JsonSerializer serializer)
 {
   JObject jo = JObject.Load(reader);
@@ -79,17 +81,10 @@ const unwrapDictionaryRead = Object.values(model.properties)
 
   ${corePropsRead.join('\n')}
 
-  var additionalProperties = jo.Properties().Where((prop) => ${nonDictionaryPropCheck.join(' || ')});
-  var coreProperties = jo.Properties().Where((prop) => ${dictionaryPropCheck.join(' || ')});
-  ${dictionaryInitializers}
-
-  foreach (var additionalProperty in additionalProperties)
-  {
-    ${unwrapDictionaryRead.join('\n')}
-  }
+  ${additionalPropertiesCode}
   return value;
 }`;
-} 
+}
 
 /**
  * Preset which adds `serialize` and `deserialize` functions to class. 
@@ -98,13 +93,13 @@ const unwrapDictionaryRead = Object.values(model.properties)
  */
 export const CSHARP_NEWTONSOFT_SERIALIZER_PRESET: CSharpPreset<CSharpOptions> = {
   class: {
-    self: ({ renderer, content, model  }) => {
+    self: ({ renderer, content, model }) => {
       renderer.addDependency('using Newtonsoft.Json;');
       renderer.addDependency('using Newtonsoft.Json.Linq;');
       renderer.addDependency('using System.Collections.Generic;');
 
-      const deserialize = renderDeserialize({model});
-      const serialize = renderSerialize({model});
+      const deserialize = renderDeserialize({ model });
+      const serialize = renderSerialize({ model });
 
       return `[JsonConverter(typeof(${model.name}Converter))]
 ${content}
