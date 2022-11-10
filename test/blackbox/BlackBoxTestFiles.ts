71001e662feabb5d0a086978a4a3559c48b8b0fe
jonaslagoni <jonas-lt@live.dk>
2022-11-10 23:20:27 +0100

    fixed linter

--- test/blackbox/BlackBoxTestFiles.ts
+++ test/blackbox/BlackBoxTestFiles.ts
@@ -6,12 +6,14 @@ import * as fs from 'fs';
  * @param folder 
  */
 function readFilesInFolder(folder: string) {
-    const fullPath = path.resolve(__dirname, `./docs/${folder}`);
-    return fs.readdirSync(fullPath).map(
-        (file) => {
-            return { file: `./docs/${folder}/${file}`, outputDirectory: `./output/${folder}/${path.parse(file).name}` };
-        }
-    );
+  // eslint-disable-next-line no-undef
+  const fullPath = path.resolve(__dirname, `./docs/${folder}`);
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
+  return fs.readdirSync(fullPath).map(
+    (file) => {
+      return { file: `./docs/${folder}/${file}`, outputDirectory: `./output/${folder}/${path.parse(file).name}` };
+    }
+  );
 }
 
 const OpenAPI3_0Files = readFilesInFolder('OpenAPI-3_0');
@@ -26,33 +28,33 @@ const AsyncAPIV2_4Files = readFilesInFolder('AsyncAPI-2_4');
 const AsyncAPIV2_5Files = readFilesInFolder('AsyncAPI-2_5');
 
 const filesToTest = [
-    ...OpenAPI3_0Files.filter(({ file }) => {
-        // Too large to process in normal blackbox testing, can be used locally test stuff.
-        return !file.includes('postman-api.json');
-    }).filter(({ file }) => {
-        // Too large to process in normal blackbox testing, can be used locally test stuff.
-        return !file.includes('twilio-1_13.json');
-    }),
-    ...AsyncAPIV2_0Files.filter(({ file }) => {
-        // Too large to process in normal blackbox testing, can be used locally test stuff.
-        return !file.includes('zbos_mqtt-all-asyncapi.json');
-    }),
-    ...AsyncAPIV2_1Files,
-    ...AsyncAPIV2_2Files,
-    ...AsyncAPIV2_3Files,
-    ...AsyncAPIV2_4Files,
-    ...AsyncAPIV2_5Files,
-    ...jsonSchemaDraft4Files.filter(({ file }) => {
-        // Too large to process in normal blackbox testing, can be used locally test stuff.
-        return !file.includes('aws-cloudformation.json');
-    }),
-    ...jsonSchemaDraft7Files.filter(({ file }) => {
-        // Too large to process in normal blackbox testing, can be used locally test stuff.
-        return !file.includes('graphql-code-generator.json');
-    }),
-    ...jsonSchemaDraft6Files.filter(({ file }) => {
-        // Too large to process in normal blackbox testing, can be used locally test stuff.
-        return !file.includes('fhir-full.json');
-    }),
+  ...OpenAPI3_0Files.filter(({ file }) => {
+    // Too large to process in normal blackbox testing, can be used locally test stuff.
+    return !file.includes('postman-api.json');
+  }).filter(({ file }) => {
+    // Too large to process in normal blackbox testing, can be used locally test stuff.
+    return !file.includes('twilio-1_13.json');
+  }),
+  ...AsyncAPIV2_0Files.filter(({ file }) => {
+    // Too large to process in normal blackbox testing, can be used locally test stuff.
+    return !file.includes('zbos_mqtt-all-asyncapi.json');
+  }),
+  ...AsyncAPIV2_1Files,
+  ...AsyncAPIV2_2Files,
+  ...AsyncAPIV2_3Files,
+  ...AsyncAPIV2_4Files,
+  ...AsyncAPIV2_5Files,
+  ...jsonSchemaDraft4Files.filter(({ file }) => {
+    // Too large to process in normal blackbox testing, can be used locally test stuff.
+    return !file.includes('aws-cloudformation.json');
+  }),
+  ...jsonSchemaDraft7Files.filter(({ file }) => {
+    // Too large to process in normal blackbox testing, can be used locally test stuff.
+    return !file.includes('graphql-code-generator.json');
+  }),
+  ...jsonSchemaDraft6Files.filter(({ file }) => {
+    // Too large to process in normal blackbox testing, can be used locally test stuff.
+    return !file.includes('fhir-full.json');
+  }),
 ];
 export default filesToTest;
