b35f0cae4c982c6fc11ab46abb359eb6687cc957
jonaslagoni <jonas-lt@live.dk>
2022-12-06 23:49:48 +0100

    add impl

--- src/generators/python/PythonConstrainer.ts
+++ src/generators/python/PythonConstrainer.ts
@@ -1,10 +1,9 @@
-import { TypeMapping } from '../../helpers';
 import { defaultEnumKeyConstraints, defaultEnumValueConstraints } from './constrainer/EnumConstrainer';
 import { defaultModelNameConstraints } from './constrainer/ModelNameConstrainer';
 import { defaultPropertyKeyConstraints } from './constrainer/PropertyKeyConstrainer';
-import { PythonOptions } from './PythonGenerator';
+import { PythonTypeMapping } from './PythonGenerator';
 
-export const PythonDefaultTypeMapping: TypeMapping<PythonOptions> = {
+export const PythonDefaultTypeMapping: PythonTypeMapping = {
   Object ({constrainedModel}): string {
     //Returning name here because all object models have been split out
     return constrainedModel.name;
