dea0b3729e8c1992b9413da0fa8f3704369a90cc
Jonas Lagoni <jonas-lt@live.dk>
2022-12-08 13:03:44 +0100

    chore: add dependency manager for Dart (#1043)

--- src/generators/dart/presets/JsonSerializablePreset.ts
+++ src/generators/dart/presets/JsonSerializablePreset.ts
@@ -10,9 +10,9 @@ export const DART_JSON_PRESET: DartPreset = {
   class: {
     self({renderer, model, content}) {
       const snakeformattedModelName = FormatHelpers.snakeCase(model.name);
-      renderer.addDependency('import \'package:json_annotation/json_annotation.dart\';');
-      renderer.addDependency(`part '${snakeformattedModelName}.g.dart';`);
-      renderer.addDependency('@JsonSerializable()');
+      renderer.dependencyManager.addDependency('import \'package:json_annotation/json_annotation.dart\';');
+      renderer.dependencyManager.addDependency(`part '${snakeformattedModelName}.g.dart';`);
+      renderer.dependencyManager.addDependency('@JsonSerializable()');
       return content;
     },
     additionalContent({model}) {
@@ -23,9 +23,9 @@ Map<String, dynamic> toJson() => _$${model.name}ToJson(this);`;
   enum: {
     self({renderer, model, content}) {
       const snakeformattedModelName = FormatHelpers.snakeCase(model.name);
-      renderer.addDependency('import \'package:json_annotation/json_annotation.dart\';');
-      renderer.addDependency(`part '${snakeformattedModelName}.g.dart';`);
-      renderer.addDependency('@JsonEnum(alwaysCreate:true)');
+      renderer.dependencyManager.addDependency('import \'package:json_annotation/json_annotation.dart\';');
+      renderer.dependencyManager.addDependency(`part '${snakeformattedModelName}.g.dart';`);
+      renderer.dependencyManager.addDependency('@JsonEnum(alwaysCreate:true)');
       return content;
     },
   }
--- test/generators/dart/DartRenderer.spec.ts
+++ test/generators/dart/DartRenderer.spec.ts
@@ -1,4 +1,5 @@
 import { defaultGeneratorOptions, DartGenerator } from '../../../src/generators';
+import { DartDependencyManager } from '../../../src/generators/dart/DartDependencyManager';
 import { DartRenderer } from '../../../src/generators/dart/DartRenderer';
 import { ConstrainedObjectModel, InputMetaModel } from '../../../src/models';
 class MockDartRenderer extends DartRenderer<ConstrainedObjectModel> {
@@ -7,7 +8,7 @@ class MockDartRenderer extends DartRenderer<ConstrainedObjectModel> {
 describe('DartRenderer', () => {
   let renderer: DartRenderer<any>;
   beforeEach(() => {
-    renderer = new MockDartRenderer(DartGenerator.defaultOptions, new DartGenerator(), [], new ConstrainedObjectModel('', undefined, '', {}), new InputMetaModel());
+    renderer = new MockDartRenderer(DartGenerator.defaultOptions, new DartGenerator(), [], new ConstrainedObjectModel('', undefined, '', {}), new InputMetaModel(), new DartDependencyManager(DartGenerator.defaultOptions));
   });
   describe('renderComments()', () => {
     test('Should be able to render comments', () => {
