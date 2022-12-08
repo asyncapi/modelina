cce502e9094b2fe6a0f5ac63972c0be25b6d8dbb
Jonas Lagoni <jonas-lt@live.dk>
2022-12-08 13:03:47 +0100

    chore: add dependency manager for Go (#1042)

--- test/generators/go/GoConstrainer.spec.ts
+++ test/generators/go/GoConstrainer.spec.ts
@@ -1,10 +1,12 @@
 import {GoDefaultTypeMapping } from '../../../src/generators/go/GoConstrainer';
 import { ConstrainedAnyModel, ConstrainedArrayModel, ConstrainedBooleanModel, ConstrainedDictionaryModel, ConstrainedEnumModel, ConstrainedFloatModel, ConstrainedIntegerModel, ConstrainedObjectModel, ConstrainedReferenceModel, ConstrainedStringModel, ConstrainedTupleModel, ConstrainedUnionModel, GoGenerator } from '../../../src';
+import { GoDependencyManager } from '../../../src/generators/go/GoDependencyManager';
 describe('GoConstrainer', () => {
+  const defaultOptions = {options: GoGenerator.defaultOptions, dependencyManager: new GoDependencyManager(GoGenerator.defaultOptions)};
   describe('ObjectModel', () => { 
     test('should render the constrained name as type', () => {
       const model = new ConstrainedObjectModel('test', undefined, '', {});
-      const type = GoDefaultTypeMapping.Object({constrainedModel: model, options: GoGenerator.defaultOptions});
+      const type = GoDefaultTypeMapping.Object({constrainedModel: model, ...defaultOptions});
       expect(type).toEqual(model.name);
     });
   });
@@ -12,42 +14,42 @@ describe('GoConstrainer', () => {
     test('should render the constrained name as type', () => {
       const refModel = new ConstrainedAnyModel('test', undefined, '');
       const model = new ConstrainedReferenceModel('test', undefined, '', refModel);
-      const type = GoDefaultTypeMapping.Reference({constrainedModel: model, options: GoGenerator.defaultOptions});
+      const type = GoDefaultTypeMapping.Reference({constrainedModel: model, ...defaultOptions});
       expect(type).toEqual(model.name);
     });
   });
   describe('Any', () => { 
     test('should render type', () => {
       const model = new ConstrainedAnyModel('test', undefined, '');
-      const type = GoDefaultTypeMapping.Any({constrainedModel: model, options: GoGenerator.defaultOptions});
+      const type = GoDefaultTypeMapping.Any({constrainedModel: model, ...defaultOptions});
       expect(type).toEqual('interface{}');
     });
   });
   describe('Float', () => { 
     test('should render type', () => {
       const model = new ConstrainedFloatModel('test', undefined, '');
-      const type = GoDefaultTypeMapping.Float({constrainedModel: model, options: GoGenerator.defaultOptions});
+      const type = GoDefaultTypeMapping.Float({constrainedModel: model, ...defaultOptions});
       expect(type).toEqual('float64');
     });
   });
   describe('Integer', () => { 
     test('should render type', () => {
       const model = new ConstrainedIntegerModel('test', undefined, '');
-      const type = GoDefaultTypeMapping.Integer({constrainedModel: model, options: GoGenerator.defaultOptions});
+      const type = GoDefaultTypeMapping.Integer({constrainedModel: model, ...defaultOptions});
       expect(type).toEqual('int');
     });
   });
   describe('String', () => { 
     test('should render type', () => {
       const model = new ConstrainedStringModel('test', undefined, '');
-      const type = GoDefaultTypeMapping.String({constrainedModel: model, options: GoGenerator.defaultOptions});
+      const type = GoDefaultTypeMapping.String({constrainedModel: model, ...defaultOptions});
       expect(type).toEqual('string');
     });
   });
   describe('Boolean', () => { 
     test('should render type', () => {
       const model = new ConstrainedBooleanModel('test', undefined, '');
-      const type = GoDefaultTypeMapping.Boolean({constrainedModel: model, options: GoGenerator.defaultOptions});
+      const type = GoDefaultTypeMapping.Boolean({constrainedModel: model, ...defaultOptions});
       expect(type).toEqual('bool');
     });
   });
@@ -55,7 +57,7 @@ describe('GoConstrainer', () => {
   describe('Tuple', () => { 
     test('should render type', () => {
       const model = new ConstrainedTupleModel('test', undefined, '', []);
-      const type = GoDefaultTypeMapping.Tuple({constrainedModel: model, options: GoGenerator.defaultOptions});
+      const type = GoDefaultTypeMapping.Tuple({constrainedModel: model, ...defaultOptions});
       expect(type).toEqual('[]interface{}');
     });
   });
@@ -64,7 +66,7 @@ describe('GoConstrainer', () => {
     test('should render type', () => {
       const arrayModel = new ConstrainedStringModel('test', undefined, 'string');
       const model = new ConstrainedArrayModel('test', undefined, '', arrayModel);
-      const type = GoDefaultTypeMapping.Array({constrainedModel: model, options: GoGenerator.defaultOptions});
+      const type = GoDefaultTypeMapping.Array({constrainedModel: model, ...defaultOptions});
       expect(type).toEqual('[]string');
     });
   });
@@ -72,7 +74,7 @@ describe('GoConstrainer', () => {
   describe('Enum', () => { 
     test('should render the constrained name as type', () => {
       const model = new ConstrainedEnumModel('test', undefined, '', []);
-      const type = GoDefaultTypeMapping.Enum({constrainedModel: model, options: GoGenerator.defaultOptions});
+      const type = GoDefaultTypeMapping.Enum({constrainedModel: model, ...defaultOptions});
       expect(type).toEqual(model.name);
     });
   });
@@ -80,7 +82,7 @@ describe('GoConstrainer', () => {
   describe('Union', () => { 
     test('should render type', () => {
       const model = new ConstrainedUnionModel('test', undefined, '', []);
-      const type = GoDefaultTypeMapping.Union({constrainedModel: model, options: GoGenerator.defaultOptions});
+      const type = GoDefaultTypeMapping.Union({constrainedModel: model, ...defaultOptions});
       expect(type).toEqual('interface{}');
     });
   });
@@ -90,7 +92,7 @@ describe('GoConstrainer', () => {
       const keyModel = new ConstrainedStringModel('test', undefined, 'string');
       const valueModel = new ConstrainedStringModel('test', undefined, 'string');
       const model = new ConstrainedDictionaryModel('test', undefined, '', keyModel, valueModel);
-      const type = GoDefaultTypeMapping.Dictionary({constrainedModel: model, options: GoGenerator.defaultOptions});
+      const type = GoDefaultTypeMapping.Dictionary({constrainedModel: model, ...defaultOptions});
       expect(type).toEqual('map[string]string');
     });
   });
