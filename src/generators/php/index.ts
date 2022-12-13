ba1d016f2788b3bcdb2e9ccb0e9475b7535ded97
Ferror <zmalcherczyk@gmail.com>
2022-12-13 18:50:16 +0100

    template

--- src/generators/php/index.ts
+++ src/generators/php/index.ts
@@ -1,3 +1,21 @@
 export * from './PhpGenerator';
 export * from './PhpFileGenerator';
-export * from './PhpRenderCompleteModelOptions';
+export { TEMPLATE_DEFAULT_PRESET } from './PhpPreset';
+export type { PhpPreset } from './PhpPreset';
+export * from './presets';
+
+export {
+  defaultEnumKeyConstraints as templateDefaultEnumKeyConstraints,
+  DefaultEnumKeyConstraints as TemplateDefaultEnumKeyConstraints,
+  defaultEnumValueConstraints as templateDefaultEnumValueConstraints
+} from './constrainer/EnumConstrainer';
+
+export {
+  DefaultModelNameConstraints as TemplateDefaultModelNameConstraints,
+  defaultModelNameConstraints as templateDefaultModelNameConstraints
+} from './constrainer/ModelNameConstrainer';
+
+export {
+  DefaultPropertyKeyConstraints as TemplateDefaultPropertyKeyConstraints,
+  defaultPropertyKeyConstraints as templateDefaultPropertyKeyConstraints
+} from './constrainer/PropertyKeyConstrainer';
