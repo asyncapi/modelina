1c2e914670a155f33091ea84ebbfce48c40c53aa
Ferror <zmalcherczyk@gmail.com>
2022-12-13 18:50:03 +0100

    template

--- src/generators/php/PhpGenerator.ts
+++ src/generators/php/PhpGenerator.ts
@@ -1,26 +1,116 @@
-import { AbstractGenerator } from '../AbstractGenerator';
-import {
-    ConstrainedMetaModel,
-    ConstrainedStringModel,
-    InputMetaModel,
-    MetaModel,
-    RenderOutput
-} from '../../models';
+import { 
+  AbstractGenerator, 
+  CommonGeneratorOptions,
+  defaultGeneratorOptions
+} from '../AbstractGenerator';
+import { ConstrainedEnumModel, ConstrainedMetaModel, ConstrainedObjectModel, InputMetaModel, MetaModel, RenderOutput } from '../../models';
+import { split, TypeMapping } from '../../helpers';
+import { PhpPreset, TEMPLATE_DEFAULT_PRESET } from './PhpPreset';
+import { ClassRenderer } from './renderers/ClassRenderer';
+import { EnumRenderer } from './renderers/EnumRenderer';
+import { isReservedTemplateKeyword } from './Constants';
+import { Logger } from '../..';
+import { constrainMetaModel, Constraints } from '../../helpers/ConstrainHelpers';
+import { TemplateDefaultConstraints, TemplateDefaultTypeMapping } from './TemplateConstrainer';
+import { DeepPartial, mergePartialAndDefault } from '../../utils/Partials';
 
-export class PhpGenerator extends AbstractGenerator {
-    constrainToMetaModel(model: MetaModel): ConstrainedMetaModel {
-        return new ConstrainedStringModel('name', {}, 'type');
+export interface TemplateOptions extends CommonGeneratorOptions<PhpPreset> {
+  typeMapping: TypeMapping<TemplateOptions>;
+  constraints: Constraints;
+}
+export interface TemplateRenderCompleteModelOptions {
+  packageName: string
+}
+export class PhpGenerator extends AbstractGenerator<TemplateOptions, TemplateRenderCompleteModelOptions> {
+  static defaultOptions: TemplateOptions = {
+    ...defaultGeneratorOptions,
+    defaultPreset: TEMPLATE_DEFAULT_PRESET,
+    typeMapping: TemplateDefaultTypeMapping,
+    constraints: TemplateDefaultConstraints
+  };
+
+  constructor(
+    options?: DeepPartial<TemplateOptions>,
+  ) {
+    const realizedOptions = mergePartialAndDefault(PhpGenerator.defaultOptions, options) as TemplateOptions;
+    super('Template', realizedOptions);
+  }
+  /**
+   * This function makes sure we split up the MetaModels accordingly to what we want to render as models.
+   */
+  splitMetaModel(model: MetaModel): MetaModel[] {
+    //These are the models that we have separate renderers for
+    const metaModelsToSplit = {
+      splitEnum: true, 
+      splitObject: true
+    };
+    return split(model, metaModelsToSplit);
+  }
+
+  constrainToMetaModel(model: MetaModel): ConstrainedMetaModel {
+    return constrainMetaModel<TemplateOptions>(
+      this.options.typeMapping, 
+      this.options.constraints, 
+      {
+        metaModel: model,
+        options: this.options,
+        constrainedName: '' //This is just a placeholder, it will be constrained within the function
+      }
+    );
+  }
+
+  /**
+   * Render a scattered model, where the source code and library and model dependencies are separated.
+   * 
+   * @param model 
+   * @param inputModel 
+   */
+  render(model: ConstrainedMetaModel, inputModel: InputMetaModel): Promise<RenderOutput> {
+    if (model instanceof ConstrainedObjectModel) {
+      return this.renderClass(model, inputModel);
+    } else if (model instanceof ConstrainedEnumModel) {
+      return this.renderEnum(model, inputModel);
+    } 
+    Logger.warn(`Template generator, cannot generate this type of model, ${model.name}`);
+    return Promise.resolve(RenderOutput.toRenderOutput({ result: '', renderedName: '', dependencies: [] }));
+  }
+
+  /**
+   * Render a complete model result where the model code, library and model dependencies are all bundled appropriately.
+   * 
+   * For Template you need to specify which package the model is placed under.
+   * 
+   * @param model 
+   * @param inputModel 
+   * @param options used to render the full output
+   */
+  async renderCompleteModel(model: ConstrainedMetaModel, inputModel: InputMetaModel, options: TemplateRenderCompleteModelOptions): Promise<RenderOutput> {
+    if (isReservedTemplateKeyword(options.packageName)) {
+      throw new Error(`You cannot use reserved Template keyword (${options.packageName}) as package name, please use another.`);
     }
 
-    render(model: MetaModel, inputModel: InputMetaModel): Promise<RenderOutput> {
-        return Promise.resolve(RenderOutput.toRenderOutput({ result: '', renderedName: 'false' }));
-    }
+    const outputModel = await this.render(model, inputModel);
+    const modelDependencies = model.getNearestDependencies().map((dependencyModel) => {
+      return `import ${options.packageName}.${dependencyModel.name};`;
+    });
+    const outputContent = `package ${options.packageName};
+${modelDependencies.join('\n')}
+${outputModel.dependencies.join('\n')}
+${outputModel.result}`; 
+    return RenderOutput.toRenderOutput({result: outputContent, renderedName: outputModel.renderedName, dependencies: outputModel.dependencies});
+  }
 
-    renderCompleteModel(model: MetaModel, inputModel: InputMetaModel, options: any): Promise<RenderOutput> {
-        return Promise.resolve(RenderOutput.toRenderOutput({ result: '', renderedName: 'false' }));
-    }
+  async renderClass(model: ConstrainedObjectModel, inputModel: InputMetaModel): Promise<RenderOutput> {
+    const presets = this.getPresets('class');
+    const renderer = new ClassRenderer(this.options, this, presets, model, inputModel);
+    const result = await renderer.runSelfPreset();
+    return RenderOutput.toRenderOutput({result, renderedName: model.name, dependencies: renderer.dependencies});
+  }
 
-    splitMetaModel(model: MetaModel): MetaModel[] {
-        return [];
-    }
+  async renderEnum(model: ConstrainedEnumModel, inputModel: InputMetaModel): Promise<RenderOutput> {
+    const presets = this.getPresets('enum'); 
+    const renderer = new EnumRenderer(this.options, this, presets, model, inputModel);
+    const result = await renderer.runSelfPreset();
+    return RenderOutput.toRenderOutput({result, renderedName: model.name, dependencies: renderer.dependencies});
+  }
 }
