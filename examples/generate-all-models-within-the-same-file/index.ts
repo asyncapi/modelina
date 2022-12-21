9d688fe9705fd82eb1a81de48fcff39508e94efc
Anay Sarkar <anaysarkar7@gmail.com>
2022-12-21 20:37:39 +0530

    generate all models file update(lint fix)

--- examples/generate-all-models-within-the-same-file/index.ts
+++ examples/generate-all-models-within-the-same-file/index.ts
@@ -1,21 +1,21 @@
-import { JavaFileGenerator } from "../../src";
-import { promises as fsPromises } from "fs";
+import { JavaFileGenerator } from '../../src';
+import { promises as fsPromises } from 'fs';
 
 const generator = new JavaFileGenerator();
 const jsonSchemaDraft7 = {
-  $schema: "http://json-schema.org/draft-07/schema#",
-  type: "object",
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  type: 'object',
   additionalProperties: false,
   properties: {
     email: {
-      type: "string",
-      enum: ["example1@test.com", "example2@test.com"],
+      type: 'string',
+      enum: ['example1@test.com', 'example2@test.com'],
     },
   },
 };
 export async function generate(): Promise<void> {
   const outputFile =
-    "./output";
+    './output';
   const models = await generator.generate(jsonSchemaDraft7);
   const modelCode = models.map((outputModel) => {
     return outputModel.result;
@@ -24,8 +24,8 @@ export async function generate(): Promise<void> {
     return outputModel.dependencies;
   });
   const uniqueImports = [...new Set(imports)];
-  const codeWithImports = `${uniqueImports.join("\n")}
-  ${modelCode.join("\n")}`;
+  const codeWithImports = `${uniqueImports.join('\n')}
+  ${modelCode.join('\n')}`;
 
   await fsPromises.writeFile(outputFile, codeWithImports);
   console.log(codeWithImports);
