70b0137a826a1f59a2f47f1d6e871bf6ca878a6f
henrikj <henrik@hjonsson.net>
2023-03-17 22:37:52 +0100

    Adds support for recordrenderer in generator and adds preset

--- src/generators/csharp/CSharpPreset.ts
+++ src/generators/csharp/CSharpPreset.ts
@@ -15,6 +15,10 @@ import {
   CSHARP_DEFAULT_ENUM_PRESET,
   EnumRenderer
 } from './renderers/EnumRenderer';
+import {
+  RecordRenderer, 
+  CSHARP_DEFAULT_RECORD_PRESET
+} from "./renderers/RecordRenderer";
 
 // Our class preset uses custom `accessor` hook to craft getter and setters.
 export interface CsharpClassPreset<O> extends ClassPreset<ClassRenderer, O> {
@@ -23,15 +27,20 @@ export interface CsharpClassPreset<O> extends ClassPreset<ClassRenderer, O> {
   ) => Promise<string> | string;
 }
 
+export interface CsharpRecordPreset<O> extends ClassPreset<RecordRenderer, O> {
+}
+
 export type ClassPresetType<O> = CsharpClassPreset<O>;
 export type EnumPresetType<O> = EnumPreset<EnumRenderer, O>;
 
 export type CSharpPreset<O = any> = Preset<{
   class: CsharpClassPreset<O>;
+  record: CsharpRecordPreset<0>;
   enum: EnumPreset<EnumRenderer, O>;
 }>;
 
 export const CSHARP_DEFAULT_PRESET: CSharpPreset<CSharpOptions> = {
   class: CSHARP_DEFAULT_CLASS_PRESET,
+  record: CSHARP_DEFAULT_RECORD_PRESET,
   enum: CSHARP_DEFAULT_ENUM_PRESET
 };
