0b3df2293cbdc7eaed0b07f84ee997412c1fd9f3
henrikj <henrik@hjonsson.net>
2023-03-17 22:37:24 +0100

    Adds a draft for a RecordRenderer

---
+++ src/generators/csharp/renderers/RecordRenderer.ts
@@ -0,0 +1,115 @@
+import { CSharpRenderer } from '../CSharpRenderer';
+import {
+  ConstrainedDictionaryModel,
+  ConstrainedObjectModel,
+  ConstrainedObjectPropertyModel
+} from '../../../models';
+import { pascalCase } from 'change-case';
+import { CsharpClassPreset } from '../CSharpPreset';
+import { CSharpOptions } from '../CSharpGenerator';
+
+/**
+ * Renderer for CSharp's `struct` type
+ *
+ * @extends CSharpRenderer
+ */
+export class RecordRenderer extends CSharpRenderer<ConstrainedObjectModel> {
+  public async defaultSelf(): Promise<string> {
+    const content = [
+      await this.renderProperties(),
+      await this.runCtorPreset(),
+      await this.renderAccessors(),
+      await this.runAdditionalContentPreset()
+    ];
+
+    if (
+      this.options?.collectionType === 'List' ||
+      this.model.containsPropertyType(ConstrainedDictionaryModel)
+    ) {
+      this.dependencyManager.addDependency('using System.Collections.Generic;');
+    }
+
+    return `public record ${this.model.name}
+{
+${this.indent(this.renderBlock(content, 2))}
+}`;
+  }
+
+  async renderProperties(): Promise<string> {
+    const properties = this.model.properties || {};
+    const content: string[] = [];
+
+    for (const property of Object.values(properties)) {
+      const rendererProperty = await this.runPropertyPreset(property);
+      content.push(rendererProperty);
+    }
+
+    return this.renderBlock(content);
+  }
+
+  async renderAccessors(): Promise<string> {
+    const properties = this.model.properties || {};
+    const content: string[] = [];
+
+    for (const property of Object.values(properties)) {
+      content.push(await this.runAccessorPreset(property));
+    }
+
+    return this.renderBlock(content, 2);
+  }
+
+  runCtorPreset(): Promise<string> {
+    return this.runPreset('ctor');
+  }
+
+  runAccessorPreset(property: ConstrainedObjectPropertyModel): Promise<string> {
+    return this.runPreset('accessor', {
+      property,
+      options: this.options,
+      renderer: this
+    });
+  }
+
+  runPropertyPreset(property: ConstrainedObjectPropertyModel): Promise<string> {
+    return this.runPreset('property', {
+      property,
+      options: this.options,
+      renderer: this
+    });
+  }
+
+  runGetterPreset(property: ConstrainedObjectPropertyModel): Promise<string> {
+    return this.runPreset('getter', {
+      property,
+      options: this.options,
+      renderer: this
+    });
+  }
+
+  runSetterPreset(property: ConstrainedObjectPropertyModel): Promise<string> {
+    return this.runPreset('setter', {
+      property,
+      options: this.options,
+      renderer: this
+    });
+  }
+}
+
+export const CSHARP_DEFAULT_RECORD_PRESET: CsharpClassPreset<CSharpOptions> = {
+  self({ renderer }) {
+    return renderer.defaultSelf();
+  },
+  async property({renderer, property, options}) {
+    const getter = await renderer.runGetterPreset(property);
+    const setter = await renderer.runSetterPreset(property);
+    return `public ${property.required ? 'required ' : ''}${property.property.type} ${pascalCase(
+        property.propertyName
+    )} { ${getter} ${setter} }`;
+  },
+  getter({ options, property }) {
+    return 'get;';
+  },
+  setter({ options, property }) {
+    return 'init;';
+  }
+};
