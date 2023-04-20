548fe3c7ce34280d192812e50a9edfc702dc3d53
Abhay Garg <54636525+prayutsu@users.noreply.github.com>
2023-04-20 13:26:23 +0530

    Update examples/python-generate-json-serializer-and-deserializer/index.spec.ts
    Co-authored-by: Jonas Lagoni <jonas-lt@live.dk>

--- examples/python-generate-json-serializer-and-deserializer/index.spec.ts
+++ examples/python-generate-json-serializer-and-deserializer/index.spec.ts
@@ -3,7 +3,7 @@ const spy = jest.spyOn(global.console, 'log').mockImplementation(() => {
 });
 import { generate } from './index';
 
-describe('Should be able to render TEMPLATE', () => {
+describe('Should be able to render JSON serialization and deserialization functions', () => {
   afterAll(() => {
     jest.restoreAllMocks();
   });
