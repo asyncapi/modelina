9039b41a62e3ab804d6143d8b72c84703087b2c4
jonas-lt@live.dk <jonas-lt@live.dk>
2023-05-20 23:31:48 +0200

    chore: fix linting errors

--- examples/php-generate-documentation-preset/index.ts
+++ examples/php-generate-documentation-preset/index.ts
@@ -1,4 +1,4 @@
-import {OutputModel, PhpGenerator, PHP_DESCRIPTION_PRESET} from '../../src';
+import { PhpGenerator, PHP_DESCRIPTION_PRESET } from '../../src';
 
 const generator: PhpGenerator = new PhpGenerator({
   presets: [PHP_DESCRIPTION_PRESET]
@@ -12,13 +12,13 @@ const jsonSchemaDraft7 = {
     email: {
       type: 'string',
       format: 'email',
-      description: 'Description for the email property',
+      description: 'Description for the email property'
     }
   }
 };
 
 export async function generate(): Promise<void> {
-  const models: OutputModel[] = await generator.generate(jsonSchemaDraft7);
+  const models = await generator.generate(jsonSchemaDraft7);
   for (const model of models) {
     console.log(model.result);
   }
