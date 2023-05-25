8e82f0dc965d5187ac8d5d350b821b01128fdb5d
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-05-26 02:28:46 +0530

    feat: add rust runtime tests

---
+++ test/runtime/runtime-rust.spec.ts
@@ -0,0 +1,12 @@
+import path from 'path';
+import { execCommand } from '../blackbox/utils/Utils';
+
+jest.setTimeout(50000);
+
+test('Rust runtime testing', async () => {
+  const compileCommand = `cd ${path.resolve(
+    __dirname,
+    './runtime-rust'
+  )} && cargo test`;
+  await execCommand(compileCommand);
+});
