2ff09d63c330f753543ef73a38e437ca7b8e2ab7
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-05-26 10:35:08 +0530

    fix: increase testing time limit

--- test/runtime/runtime-rust.spec.ts
+++ test/runtime/runtime-rust.spec.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { execCommand } from '../blackbox/utils/Utils';
 
-jest.setTimeout(50000);
+jest.setTimeout(100000);
 
 test('Rust runtime testing', async () => {
   const compileCommand = `cd ${path.resolve(
