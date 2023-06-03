0175034f1d5d3e422265b0ef8a799dd2bc4214a6
SumantxD <st72365@gmail.com>
2023-06-02 21:55:29 -0500

    added the required changes

--- test/runtime/runtime-kotlin.spec.ts
+++ test/runtime/runtime-kotlin.spec.ts
@@ -4,8 +4,19 @@ import path from 'path';
 jest.setTimeout(50000);
 
 test('Kotlin runtime testing', () => {
-  const testCommand = `cd ${path.resolve(__dirname, './runtime-kotlin')} && ./gradlew test`;
+  const buildCommand = `cd ${path.resolve(
+    __dirname,
+    './runtime-kotlin'
+  )} && ./gradlew build`;
+  const testCommand = `cd ${path.resolve(
+    __dirname,
+    './runtime-kotlin'
+  )} && ./gradlew test`;
+
   try {
+    execSync(buildCommand, { stdio: 'inherit' });
+    console.log('Kotlin project built successfully.');
+
     execSync(testCommand, { stdio: 'inherit' });
     console.log('Kotlin test execution successful.');
   } catch (error) {
--- test/runtime/runtime-kotlin.ts
+++ test/runtime/runtime-kotlin.ts
@@ -1,28 +1,34 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+// eslint-disable-next-line no-undef
 const currentDirPath = __dirname;
 const outputDir = path.resolve(
   currentDirPath,
   './runtime-kotlin/src/test/kotlin'
 );
-const packageName = 'com.mycompany.app';
 
-// Define the Kotlin code as a string template
-const kotlinCode = `
-package ${packageName}
+// Define the TypeScript code as a string template
+const typescriptCode = `
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 
-class MyClass {
-    val message: String = "Hello, Kotlin!"
-  
-    fun printMessage(name: String) {
-        println("Hello, " + name)
-    }
+data class Address @JsonCreator constructor(
+    @JsonProperty("street_name") val streetName: String,
+    @JsonProperty("house_number") val houseNumber: Int,
+    @JsonProperty("marriage") val marriage: Boolean,
+    @JsonProperty("members") val members: Any?,
+    @JsonProperty("array_type") val arrayType: List<Any>,
+    @JsonProperty("nestedObject") val nestedObject: NestedObject
+) {
+    data class NestedObject @JsonCreator constructor(
+        @JsonProperty("test") val test: String
+    )
 }
 `;
 
 // Create the output directory if it doesn't exist
 fs.mkdirSync(outputDir, { recursive: true });
 
-// Write the Kotlin code to a file
-fs.writeFileSync(path.join(outputDir, 'MyClass.kt'), kotlinCode);
+// Write the TypeScript code to a file
+fs.writeFileSync(path.join(outputDir, 'Address.kt'), typescriptCode);
