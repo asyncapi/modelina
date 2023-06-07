26566b7c3a739325da28776faaa95e39ee94c48e
SumantxD <st72365@gmail.com>
2023-06-06 21:11:03 -0500

    removed the .gradle directory and added the necessary build command in runtime-kotlin.spec.ts

--- test/runtime/runtime-kotlin.spec.ts
+++ test/runtime/runtime-kotlin.spec.ts
@@ -4,9 +4,16 @@ import path from 'path';
 jest.setTimeout(50000);
 
 test('Kotlin runtime testing', async () => {
-  const compileCommand = `cd ${path.resolve(
+  //The 'build' command here
+  const buildCommand = `cd ${path.resolve(
+    __dirname,
+    './runtime-kotlin'
+  )} && ./gradlew build`;
+  await execCommand(buildCommand);
+  //The 'test' command here
+  const testCommand = `cd ${path.resolve(
     __dirname,
     './runtime-kotlin'
   )} && ./gradlew test`;
-  await execCommand(compileCommand);
+  await execCommand(testCommand);
 });
