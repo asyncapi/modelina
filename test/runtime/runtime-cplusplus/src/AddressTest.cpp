bac406c5e0a9b6598756e362f1b4c3d955976c8e
Kartik Jolapara <kjmickey003@gmail.com>
2023-06-16 23:32:22 +0530

    fix: clean unnecessary testing of the own implementation of serialization
    Signed-off-by: Kartik Jolapara <kjmickey003@gmail.com>

--- test/runtime/runtime-cplusplus/src/AddressTest.cpp
+++ test/runtime/runtime-cplusplus/src/AddressTest.cpp
@@ -10,7 +10,7 @@
 
 using json = nlohmann::json;
 
-TEST_CASE("Should be able to serialize C++ model")
+TEST_CASE("Should make an object of the generated class")
 {
     AsyncapiModels::address address;
     address.street_name = "Test address 2";
@@ -24,61 +24,4 @@ TEST_CASE("Should be able to serialize C++ model")
     AsyncapiModels::nested_object obj;
     obj.test = "test";
     address.nested_object = obj;
-
-    std::vector<std::string> stringVector;
-    for (const auto &item : array_type)
-    {
-        if (auto str = std::get_if<std::string>(&item))
-        {
-            stringVector.push_back(*str);
-        }
-    }
-
-    json jsonData;
-    jsonData["streetName"] = address.street_name;
-    jsonData["houseNumber"] = address.house_number;
-    jsonData["marriage"] = address.marriage.value();
-    jsonData["members"] = std::get<double>(address.members.value());
-    jsonData["arrayType"] = stringVector;
-    jsonData["nestedObject"] = {{"test", address.nested_object.value().test.value()}};
-
-    std::string jsonStr = jsonData.dump();
-    CHECK(jsonStr.length() > 0);
-}
-
-TEST_CASE("Should not contain additional properties when serialized")
-{
-    AsyncapiModels::address address;
-    address.street_name = "Test address 2";
-    address.house_number = 2.0;
-    address.marriage = true;
-    address.members = 2.0;
-    std::vector<std::variant<std::string, double, std::any>> array_type;
-    array_type.push_back(2.0);
-    array_type.push_back(std::string("test"));
-    address.array_type = array_type;
-    AsyncapiModels::nested_object obj;
-    obj.test = "test";
-    address.nested_object = obj;
-
-    std::vector<std::string> stringVector;
-    for (const auto &item : array_type)
-    {
-        if (auto str = std::get_if<std::string>(&item))
-        {
-            stringVector.push_back(*str);
-        }
-    }
-
-    json jsonData;
-    jsonData["streetName"] = address.street_name;
-    jsonData["houseNumber"] = address.house_number;
-    jsonData["marriage"] = address.marriage.value();
-    jsonData["members"] = std::get<double>(address.members.value());
-    jsonData["arrayType"] = stringVector;
-    jsonData["nestedObject"] = {{"test", address.nested_object.value().test.value()}};
-
-    std::string jsonStr = jsonData.dump();
-    std::cout << jsonStr << std::endl;
-    CHECK(jsonStr.find("additionalProperties") == std::string::npos);
 }
