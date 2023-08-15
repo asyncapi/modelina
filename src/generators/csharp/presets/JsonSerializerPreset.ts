42f25139be5f496c3e437d187bcf0fb9cbdcb58d
Jean-François Côté <jeanfrancois.cote@flo.com>
2023-08-15 14:00:54 -0400

    Fix problem with json serializer option

--- src/generators/csharp/presets/JsonSerializerPreset.ts
+++ src/generators/csharp/presets/JsonSerializerPreset.ts
@@ -20,7 +20,7 @@ function renderSerializeProperty(
     model.property instanceof ConstrainedReferenceModel &&
     model.property.ref instanceof ConstrainedEnumModel
   ) {
-    value = `${model.property.type}.GetValue()`;
+    value = `value.${model.property.type}.GetValue()`;
   }
   return `JsonSerializer.Serialize(writer, ${value}, options);`;
 }
@@ -123,7 +123,7 @@ function renderDeserializeProperty(model: ConstrainedObjectPropertyModel) {
     model.property instanceof ConstrainedReferenceModel &&
     model.property.ref instanceof ConstrainedEnumModel
   ) {
-    return `${model.property.name}Extension.To${model.property.name}(JsonSerializer.Deserialize<dynamic>(ref reader, options))`;
+    return `${model.property.name}Extensions.To${model.property.name}(JsonSerializer.Deserialize<dynamic>(ref reader, options))`;
   }
   return `JsonSerializer.Deserialize<${model.property.type}>(ref reader, options)`;
 }
