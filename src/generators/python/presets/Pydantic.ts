a14702c51a07016ef0f79f1765aa9fdb7af49a6c
Kenneth Aasan <k.aasan@sportradar.com>
2023-11-21 11:32:37 +0100

    feat: render python union in pydantic in the pre 3.10 way

--- src/generators/python/presets/Pydantic.ts
+++ src/generators/python/presets/Pydantic.ts
@@ -1,10 +1,11 @@
+import { ConstrainedUnionModel } from '../../../models';
 import { PythonOptions } from '../PythonGenerator';
 import { ClassPresetType, PythonPreset } from '../PythonPreset';
 
 const PYTHON_PYDANTIC_CLASS_PRESET: ClassPresetType<PythonOptions> = {
   async self({ renderer, model }) {
     renderer.dependencyManager.addDependency(
-      'from typing import Optional, Any'
+      'from typing import Optional, Any, Union'
     );
     renderer.dependencyManager.addDependency(
       'from pydantic import BaseModel, Field'
@@ -18,9 +19,19 @@ const PYTHON_PYDANTIC_CLASS_PRESET: ClassPresetType<PythonOptions> = {
     );
   },
   property(params) {
-    const type = params.property.required
-      ? params.property.property.type
-      : `Optional[${params.property.property.type}]`;
+    let type = params.property.property.type;
+
+    if (params.property.property instanceof ConstrainedUnionModel) {
+      const unionTypes = params.property.property.union.map(
+        (unionModel) => unionModel.type
+      );
+      type = `Union[${unionTypes.join(', ')}]`;
+    }
+
+    if (!params.property.required) {
+      type = `Optional[${type}]`;
+    }
+
     const alias = params.property.property.originalInput['description']
       ? `alias='''${params.property.property.originalInput['description']}'''`
       : '';
