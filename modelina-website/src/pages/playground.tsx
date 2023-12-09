6562394cccc165fa9f6f7717ecd9fcac66998915
devilkiller-ag <ashmitgupta.social@gmail.com>
2023-12-09 07:04:38 +0530

    Added Seprate Display for mobile visitors to playground

--- modelina-website/src/pages/playground.tsx
+++ modelina-website/src/pages/playground.tsx
@@ -1,10 +1,25 @@
+"use client"
+import { useEffect, useState } from 'react';
 import Playground from '@/components/playground/Playground';
 import GenericLayout from '@/components/layouts/GenericLayout';
 import { PlaygroundContextProvider } from '@/components/contexts/PlaygroundContext';
 
 export default function PlaygroundPage() {
+  const [width, setWidth] = useState(window.innerWidth);
   const description = 'Try out Modelina and see a fraction of what it can do.';
   const image = '/img/social/modelina-card.jpg';
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, []);
 
   return (
     <GenericLayout
@@ -16,9 +31,16 @@ export default function PlaygroundPage() {
       padding = ""
       footerPadding = "mb-0"
     >
-      <PlaygroundContextProvider>
-        <Playground />
-      </PlaygroundContextProvider>
+      {
+        width > 768 ? 
+          <PlaygroundContextProvider>
+            <Playground />
+          </PlaygroundContextProvider>
+        :
+          <div className='h-screen w-full p-4 flex justify-center items-center '>
+            Please revisit from the desktop to use the Modelina Playground.
+          </div>
+      }
     </GenericLayout>
   );
 }
