16bfc02bbdb4374392c5f8a1dd33877dc2a35de4
devilkiller-ag <ashmitgupta.social@gmail.com>
2023-12-12 09:53:38 +0530

    🛠️  GoGeneratorOptions: changed react class component into functional component

--- modelina-website/src/components/playground/options/GoGeneratorOptions.tsx
+++ modelina-website/src/components/playground/options/GoGeneratorOptions.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useContext, useCallback } from 'react';
 import { debounce } from 'lodash';
 import { PlaygroundGoConfigContext } from '@/components/contexts/PlaygroundConfigContext';
 import InfoModal from '@/components/InfoModal';
@@ -13,60 +13,54 @@ interface GoGeneratorState {
 
 export const defaultState: GoGeneratorState = {};
 
-class GoGeneratorOptions extends React.Component<
-  GoGeneratorOptionsProps,
-  GoGeneratorState
-> {
-  static contextType = PlaygroundGoConfigContext;
-  declare context: React.ContextType<typeof PlaygroundGoConfigContext>;
-  constructor(props: any) {
-    super(props);
-    this.state = defaultState;
-    this.debouncedSetNewConfig = this.debouncedSetNewConfig.bind(this);
-    this.onChangePackageName = this.onChangePackageName.bind(this);
-  }
+const GoGeneratorOptions: React.FC<GoGeneratorOptionsProps> = ({ setNewConfig }) => {
+  const context = useContext(PlaygroundGoConfigContext);
+  const [state, setState] = useState<GoGeneratorState>(defaultState);
 
-  debouncedSetNewConfig = debounce(this.props.setNewConfig as (queryKey: string, queryValue: string) => void, 500);
+  const debouncedSetNewConfig = debounce(
+    (queryKey: string, queryValue: string) => setNewConfig?.(queryKey, queryValue),
+    500
+  );
 
-  componentDidMount(): void {
-    this.setState({ ...this.state, packageName: this.context?.goPackageName });
-  }
+  useEffect(() => {
+    setState({ ...state, packageName: context?.goPackageName });
+  }, [context]);
 
-  onChangePackageName(event: any) {
-    this.setState({ ...this.state, packageName: event.target.value });
-    if (this.props.setNewConfig) {
-      this.debouncedSetNewConfig('goPackageName', event.target.value);
-    }
-  }
+  const onChangePackageName = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const packageName = event.target.value;
+      setState({ ...state, packageName });
+      setNewConfig && debouncedSetNewConfig('goPackageName', packageName);
+    },
+    [setNewConfig, debouncedSetNewConfig, state]
+  );
 
-
-  render() {
-    return (
-      <ul className="flex flex-col">
-        <h3 className="py-2 w-full text-left border-b-[1px] border-gray-700">
-          Go Specific options
-        </h3>
-        <li className='flex gap-1 items-center'>
+  return (
+    <ul className="flex flex-col">
+      <h3 className="py-2 w-full text-left border-b-[1px] border-gray-700">
+        Go Specific options
+      </h3>
+      <li className='flex gap-1 items-center'>
         <InfoModal text="Package Name :">
-            <p>
+          <p>
             In Go, a package name is used to organize code into logical groups or containers. It serves as a namespace for the code elements within it and helps in avoiding naming conflicts.
-            </p>
-          </InfoModal>
-          <label className="flex flex-grow gap-1 items-center py-2 justify-between cursor-pointer">
-            <span className="mt-1 max-w-2xl text-sm text-gray-500">
-              Package Name
-            </span>
-            <input
-              type="text"
-              className="form-input w-[90%] rounded-md border-gray-300 cursor-pointer font-regular text-md text-gray-700 hover:bg-gray-50 focus-within:text-gray-900"
-              name="goPackageName"
-              value={this.state?.packageName}
-              onChange={this.onChangePackageName}
-            />
-          </label>
-        </li>
-      </ul>
-    );
-  }
-}
+          </p>
+        </InfoModal>
+        <label className="flex flex-grow gap-1 items-center py-2 justify-between cursor-pointer">
+          <span className="mt-1 max-w-2xl text-sm text-gray-500">
+            Package Name
+          </span>
+          <input
+            type="text"
+            className="form-input w-[90%] rounded-md border-gray-300 cursor-pointer font-regular text-md text-gray-700 hover:bg-gray-50 focus-within:text-gray-900"
+            name="goPackageName"
+            value={state?.packageName}
+            onChange={onChangePackageName}
+          />
+        </label>
+      </li>
+    </ul>
+  );
+};
+
 export default GoGeneratorOptions;
