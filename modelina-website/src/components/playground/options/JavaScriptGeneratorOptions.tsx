e04ddeb9cb94eadec8009648f35e7df3767d9709
devilkiller-ag <ashmitgupta.social@gmail.com>
2023-12-12 10:18:28 +0530

    🛠️  JavaScriptGeneratorOptions: changed react class component into functional component

--- modelina-website/src/components/playground/options/JavaScriptGeneratorOptions.tsx
+++ modelina-website/src/components/playground/options/JavaScriptGeneratorOptions.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { PlaygroundJavaScriptConfigContext } from '@/components/contexts/PlaygroundConfigContext';
 
 interface JavaScriptGeneratorOptionsProps {
@@ -9,28 +9,22 @@ interface JavaScriptGeneratorState {}
 
 export const defaultState: JavaScriptGeneratorState = {};
 
-class JavaScriptGeneratorOptions extends React.Component<
-  JavaScriptGeneratorOptionsProps,
-  JavaScriptGeneratorState
-> {
-  static contextType = PlaygroundJavaScriptConfigContext;
-  declare context: React.ContextType<typeof PlaygroundJavaScriptConfigContext>;
-  constructor(props: any) {
-    super(props);
-    this.state = defaultState;
-  }
+const JavaScriptGeneratorOptions: React.FC<JavaScriptGeneratorOptionsProps> = ({
+  setNewConfig,
+}) => {
+  const context = useContext(PlaygroundJavaScriptConfigContext);
+  const [state, setState] = useState<JavaScriptGeneratorState>(defaultState);
 
-  render() {
-    return (
-      <ul className="flex flex-col">
-        <h3 className="py-2 w-full text-left border-b-[1px] border-gray-700">
-          JavaScript Specific options
-        </h3>
-        <span className="mt-1 max-w-2xl text-sm text-gray-500">
-          Currently no options are available
-        </span>
-      </ul>
-    );
-  }
-}
+  return (
+    <ul className="flex flex-col">
+      <h3 className="py-2 w-full text-left border-b-[1px] border-gray-700">
+        JavaScript Specific options
+      </h3>
+      <span className="mt-1 max-w-2xl text-sm text-gray-500">
+        Currently no options are available
+      </span>
+    </ul>
+  );
+};
+
 export default JavaScriptGeneratorOptions;
