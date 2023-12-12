e6f67c0b07d7d5edea62a34ce9b6ecac973fbbf0
devilkiller-ag <ashmitgupta.social@gmail.com>
2023-12-12 10:46:28 +0530

    🛠️  RustGeneratorOptions: changed react class component into functional component

--- modelina-website/src/components/playground/options/RustGeneratorOptions.tsx
+++ modelina-website/src/components/playground/options/RustGeneratorOptions.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { PlaygroundRustConfigContext } from '@/components/contexts/PlaygroundConfigContext';
 
 interface RustGeneratorOptionsProps {
@@ -9,28 +9,20 @@ interface RustGeneratorState {}
 
 export const defaultState: RustGeneratorState = {};
 
-class RustGeneratorOptions extends React.Component<
-  RustGeneratorOptionsProps,
-  RustGeneratorState
-> {
-  static contextType = PlaygroundRustConfigContext;
-  declare context: React.ContextType<typeof PlaygroundRustConfigContext>;
-  constructor(props: any) {
-    super(props);
-    this.state = defaultState;
-  }
+const RustGeneratorOptions: React.FC<RustGeneratorOptionsProps> = () => {
+  const context = useContext(PlaygroundRustConfigContext);
+  const [state, setState] = useState<RustGeneratorState>(defaultState);
 
-  render() {
-    return (
-      <ul className="flex flex-col">
-        <h3 className="py-2 w-full text-left border-b-[1px] border-gray-700">
-          Rust Specific options
-        </h3>
-        <span className="mt-1 max-w-2xl text-sm text-gray-500">
-          Currently no options are available
-        </span>
-      </ul>
-    );
-  }
-}
+  return (
+    <ul className="flex flex-col">
+      <h3 className="py-2 w-full text-left border-b-[1px] border-gray-700">
+        Rust Specific options
+      </h3>
+      <span className="mt-1 max-w-2xl text-sm text-gray-500">
+        Currently no options are available
+      </span>
+    </ul>
+  );
+};
+
 export default RustGeneratorOptions;
