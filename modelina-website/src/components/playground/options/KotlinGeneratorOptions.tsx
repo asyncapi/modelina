eabbaf513e0cbdbc1798d313a9d9a44cd9ba1074
devilkiller-ag <ashmitgupta.social@gmail.com>
2023-12-12 10:29:23 +0530

    🛠️  JavaScriptGeneratorOptions: changed react class component into functional component

--- modelina-website/src/components/playground/options/KotlinGeneratorOptions.tsx
+++ modelina-website/src/components/playground/options/KotlinGeneratorOptions.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { debounce } from 'lodash';
 import { PlaygroundKotlinConfigContext } from '@/components/contexts/PlaygroundConfigContext';
 import InfoModal from '@/components/InfoModal';
@@ -13,59 +13,51 @@ interface KotlinGeneratorState {
 
 export const defaultState: KotlinGeneratorState = {};
 
-class KotlinGeneratorOptions extends React.Component<
-  KotlinGeneratorOptionsProps,
-  KotlinGeneratorState
-> {
-  static contextType = PlaygroundKotlinConfigContext;
-  declare context: React.ContextType<typeof PlaygroundKotlinConfigContext>;
-  constructor(props: any) {
-    super(props);
-    this.state = defaultState;
-    this.debouncedSetNewConfig = this.debouncedSetNewConfig.bind(this);
-    this.onChangePackageName = this.onChangePackageName.bind(this);
-  }
+const KotlinGeneratorOptions: React.FC<KotlinGeneratorOptionsProps> = ({ setNewConfig }) => {
+  const context = useContext(PlaygroundKotlinConfigContext);
+  const [state, setState] = useState<KotlinGeneratorState>(defaultState);
 
-  debouncedSetNewConfig = debounce(this.props.setNewConfig as (queryKey: string, queryValue: string) => void, 500);
-
-  componentDidMount(): void {
-    this.setState({ ...this.state, packageName: this.context?.kotlinPackageName });
-  }
-
-  onChangePackageName(event: any) {
-    this.setState({ ...this.state, packageName: event.target.value });
-    if (this.props.setNewConfig) {
-      this.debouncedSetNewConfig('kotlinPackageName', event.target.value);
-    }
-  }
-
-  render() {
-    return (
-      <ul className="flex flex-col">
-        <h3 className="py-2 w-full text-left border-b-[1px] border-gray-700">
-          Kotlin Specific options
-        </h3>
-        <li className='flex gap-1 items-center'>
+  const debouncedSetNewConfig = debounce(
+    (queryKey: string, queryValue: string) => setNewConfig?.(queryKey, queryValue),
+    500
+  );
+  
+  const onChangePackageName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const packageName = event.target.value;
+    setState({ ...state, packageName });
+    setNewConfig && debouncedSetNewConfig('kotlinPackageName', packageName);
+  };
+  
+    useEffect(() => {
+      setState({ ...state, packageName: context?.kotlinPackageName });
+    }, [context]);
+  
+  return (
+    <ul className="flex flex-col">
+      <h3 className="py-2 w-full text-left border-b-[1px] border-gray-700">
+        Kotlin Specific options
+      </h3>
+      <li className='flex gap-1 items-center'>
         <InfoModal text="Package Name :">
-            <p>
+          <p>
             In Kotlin, a package name is used to organize classes, functions, and other code elements into logical groups or containers. It helps in avoiding naming conflicts and provides a way to structure your code.
-            </p>
-          </InfoModal>
-          <label className="flex flex-grow gap-1 items-center py-2 justify-between cursor-pointer">
-            <span className="mt-1 max-w-2xl text-sm text-gray-500">
-              Package Name
-            </span>
-            <input
-              type="text"
-              className="form-input w-[90%] rounded-md border-gray-300 cursor-pointer font-regular text-md text-gray-700 hover:bg-gray-50 focus-within:text-gray-900"
-              name="kotlinPackageName"
-              value={this.state?.packageName}
-              onChange={this.onChangePackageName}
-            />
-          </label>
-        </li>
-      </ul>
-    );
-  }
-}
+          </p>
+        </InfoModal>
+        <label className="flex flex-grow gap-1 items-center py-2 justify-between cursor-pointer">
+          <span className="mt-1 max-w-2xl text-sm text-gray-500">
+            Package Name
+          </span>
+          <input
+            type="text"
+            className="form-input w-[90%] rounded-md border-gray-300 cursor-pointer font-regular text-md text-gray-700 hover:bg-gray-50 focus-within:text-gray-900"
+            name="kotlinPackageName"
+            value={state?.packageName}
+            onChange={onChangePackageName}
+          />
+        </label>
+      </li>
+    </ul>
+  );
+};
+
 export default KotlinGeneratorOptions;
