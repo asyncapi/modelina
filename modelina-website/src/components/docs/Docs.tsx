5d5bf2c2908dd3422444da7e1d88f1a5e810b4b1
Asish Kumar <officialasishkumar@gmail.com>
2024-01-04 22:14:57 +0530

    fix: highlight selected doc in website
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

--- modelina-website/src/components/docs/Docs.tsx
+++ modelina-website/src/components/docs/Docs.tsx
@@ -25,7 +25,7 @@ const Docs: React.FC<ModelinaDocsProps> = ({ source, slug, router }) => {
    * Render the menu items dynamically in a depth first approach
    */
   const renderMenuTree = (value: any, isRoot: boolean) => {
-    const isSelected = value.slug === `/docs/${slug}`;
+    const isSelected = value.slug === `${slug}`;
     if (value.type === 'dir') {
       const hasRootReadme = value.content !== null;
       let headerReadme;
