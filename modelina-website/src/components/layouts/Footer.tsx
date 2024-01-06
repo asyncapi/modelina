89a0f6e21866f71160136f0f52eb0efea532131a
Asish Kumar <87874775+officialasishkumar@users.noreply.github.com>
2024-01-06 20:43:26 +0530

    docs: updated footer with heart emoji (#1728)

--- modelina-website/src/components/layouts/Footer.tsx
+++ modelina-website/src/components/layouts/Footer.tsx
@@ -225,7 +225,7 @@ export default function Footer() {
           <div className="w-full sm:w-2/3">
             <p className="text-left text-base leading-6 mb-3 text-cool-gray">
               Made with{' '}
-              <span className="font-mono text-secondary-500">:love:</span> by
+              <span>❤️</span> by
               the AsyncAPI Initiative.
             </p>
             <p className="text-left text-sm leading-6 text-cool-gray w-full sm:w-2/3">
