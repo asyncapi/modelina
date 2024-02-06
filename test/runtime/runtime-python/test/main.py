2346cd0b00367021c1a215d730906f82566b7e3d
AkhilJ321 <jamwalakhil936@gmail.com>
2024-02-06 23:35:10 +0530

    modified the tests

--- test/runtime/runtime-python/test/main.py
+++ test/runtime/runtime-python/test/main.py
@@ -1,23 +1,52 @@
 import unittest
-import json
 from main.Address import Address
 from main.NestedObject import NestedObject
-
+import json
+  
+class AddressData:
+    def __init__(self, data):
+        for key,value in data.items():
+            setattr(self, key, value)
+class Data:
+    def __init__(self, data):
+        self.__dict__ = data
+data =    {
+            'streetName': 'Test Street',
+            'houseNumber': 123,
+            'marriage': True,
+            'members': 3,
+            'arrayType': ['test1', 'test2'],
+            'nestedObject': {'test': 'test'},
+            'additionalProperties': {'prop': 'value'}
+        }
+data_obj = Data(data)       
 class TestAddress(unittest.TestCase):
-
     def setUp(self):
-        nested_obj = NestedObject("test")
-        self.address = Address("Test address 2", 2, True, 2, [2, "test"], nested_obj)
+        self.address = Address(data_obj)
+        
 
-    def test_serialize_model(self):
-        json_str = self.address.to_json()
-        self.assertIsNotNone(json_str)
-        self.assertGreater(len(json_str), 0)
+    def test_serializeToJson(self):
+        json_str =self.address.serializeToJson()
+        
+        self.assertIsInstance(json_str, str)    
 
-    def test_no_additional_properties(self):
-        json_str = self.address.to_json()
-        self.assertNotIn('additionalProperties', json_str)
+    def test_deserializeFromJson(self):
+        json_str = self.address.serializeToJson()
+        json_obj = json.loads(json_str)
+        # Strip leading underscore from the key
+        json_obj = {k[1:]: v for k, v in json_obj.items()}
+        data_obj = AddressData(json_obj)
+        address = Address(data_obj)
+        self.assertIsInstance(address, Address)
+        self.assertEqual(address.streetName, self.address.streetName)
+        self.assertEqual(address.houseNumber, self.address.houseNumber)
+        self.assertEqual(address.marriage, self.address.marriage)
+        self.assertEqual(address.members, self.address.members)
+        self.assertEqual(address.arrayType, self.address.arrayType)
+        self.assertEqual(address.nestedObject, self.address.nestedObject)
+        self.assertEqual(address.additionalProperties, self.address.additionalProperties)    
+    
+
 
 if __name__ == '__main__':
     unittest.main()
-    
