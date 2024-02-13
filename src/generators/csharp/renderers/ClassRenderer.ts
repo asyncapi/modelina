8c8282c2840250101c4380a931bac9d9d9279c64
Moritz Kalwa <moritz.kalwa@zweitag.de>
2024-02-13 19:20:15 +0100

    feat: enable renderers to handle constants

--- src/generators/csharp/renderers/ClassRenderer.ts
+++ src/generators/csharp/renderers/ClassRenderer.ts
@@ -115,10 +115,20 @@ export const CSHARP_DEFAULT_CLASS_PRESET: CsharpClassPreset<CSharpOptions> = {
       const getter = await renderer.runGetterPreset(property);
       const setter = await renderer.runSetterPreset(property);
 
+      if (property.property.options.const) {
+        return `public const ${property.property.type} ${pascalCase(
+          property.propertyName
+        )} { ${getter} } = ${property.property.options.const.value};`;
+      }
+
       const semiColon = nullablePropertyEnding !== '' ? ';' : '';
       return `public ${property.property.type} ${pascalCase(
         property.propertyName
       )} { ${getter} ${setter} }${nullablePropertyEnding}${semiColon}`;
+    }
+
+    if (property.property.options.const) {
+      return `private const ${property.property.type} ${property.propertyName} = ${property.property.options.const.value};`;
     }
     return `private ${property.property.type} ${property.propertyName}${nullablePropertyEnding};`;
   },
