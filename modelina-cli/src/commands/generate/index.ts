bd31889d9d3b33b66484ef5e989477a8243f99fa
devilkiller-ag <ashmitgupta.social@gmail.com>
2024-02-23 22:28:13 +0530

    removed boilerplate codes

--- modelina-cli/src/commands/generate/index.ts
+++ modelina-cli/src/commands/generate/index.ts
@@ -2,7 +2,7 @@ import ModelinaCommand from '../../base';
 import { Help } from '@oclif/core';
 
 export default class Generate extends ModelinaCommand {
-  static description = 'Generate typed models or other things like clients, applications or docs using AsyncAPI Generator templates.';
+  static description = 'Generate models using AsyncAPI Modelina.';
   async run() {
     const help = new Help(this.config);
     help.showHelp(['generate', '--help']);
