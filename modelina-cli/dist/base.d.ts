910b468a9a6cd6880a33ec5d019d57d446f30862
devilkiller-ag <ashmitgupta.social@gmail.com>
2024-02-28 19:18:27 +0530

    removed files in dist

--- modelina-cli/dist/base.d.ts
+++
@@ -1,6 +0,0 @@
-import { Command } from '@oclif/core';
-export default abstract class ModelinaCommand extends Command {
-    catch(err: Error & {
-        exitCode?: number;
-    }): Promise<any>;
-}
--- modelina-cli/dist/commands/generate/index.d.ts
+++
@@ -1,5 +0,0 @@
-import Command from '../../base.js';
-export default class Generate extends Command {
-    static description: string;
-    run(): Promise<void>;
-}
