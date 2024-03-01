4ddc1817efdacda35620a4b125e08af397891ce9
souvik <souvikde.ns@gmail.com>
2024-03-01 19:01:39 +0530

    feat: add json serialisation preset for go

---
+++ src/generators/go/presets/CommonPreset.ts
@@ -0,0 +1,71 @@
+import { GoPreset } from '../GoPreset';
+import { GoRenderer } from '../GoRenderer';
+import {
+  ConstrainedDictionaryModel,
+  ConstrainedObjectPropertyModel,
+  ConstrainedEnumModel
+} from '../../../models';
+
+export interface GoCommonPresetOption {
+  addJsonTag: boolean;
+}
+
+function renderJSONTag({
+  field
+}: {
+  field: ConstrainedObjectPropertyModel;
+}): string {
+  if (
+    field.property instanceof ConstrainedDictionaryModel &&
+    field.property.serializationType === 'unwrap'
+  ) {
+    return `json:"-"`;
+  }
+  return `json:"${field.unconstrainedPropertyName}"`;
+}
+
+function renderMarshallingFunctions({
+  model,
+  renderer
+}: {
+  model: ConstrainedEnumModel;
+  renderer: GoRenderer<any>;
+}): string {
+  renderer.dependencyManager.addDependency('encoding/json');
+  return `
+  func (op *${model.name}) UnmarshalJSON(raw []byte) error {
+    var v any
+    if err := json.Unmarshal(raw, &v); err != nil {
+    return err
+    }
+    *op = ValuesTo${model.name}[v]
+    return nil
+  }
+
+  func (op ${model.name}) MarshalJSON() ([]byte, error) {
+    return json.Marshal(op.Value())
+  }  
+  `;
+}
+
+export const GO_COMMON_PRESET: GoPreset<GoCommonPresetOption> = {
+  struct: {
+    field: ({ content, field, options }) => {
+      const blocks: string[] = [];
+      if (options.addJsonTag) {
+        blocks.push(renderJSONTag({ field }));
+      }
+      return `${content} \`${blocks.join(' ')}\``;
+    }
+  },
+  enum: {
+    self({ content, model, renderer, options }) {
+      const blocks: string[] = [];
+      if (options.addJsonTag) {
+        blocks.push(renderMarshallingFunctions({ model, renderer }));
+      }
+
+      return `${content}\n ${blocks.join('\n')}`;
+    }
+  }
+};
---
+++ test/generators/go/presets/CommonPreset.spec.ts
@@ -0,0 +1,10 @@
+import {GoGenerator, GO_COMMON_PRESET, GoCommonPresetOption} from '../../../../src/generators'
+
+
+describe('Go_COMMON_PRESET', () => {
+    let generator: GoGenerator
+    beforeEach(() => {
+        generator = new GoGenerator({presets: [GO_COMMON_PRESET]})
+    })
+    test('')
+})
