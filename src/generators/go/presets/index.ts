6c8b7934198fcceffdaeff2cc0dd0510e241a5ab
souvik <souvikde.ns@gmail.com>
2024-03-04 13:25:10 +0530

    test: add tests

--- src/generators/go/presets/index.ts
+++ src/generators/go/presets/index.ts
@@ -1,1 +1,1 @@
-export * from './CommonPreset'
+export * from './CommonPreset';
--- test/generators/go/presets/CommonPreset.spec.ts
+++ test/generators/go/presets/CommonPreset.spec.ts
@@ -4,12 +4,38 @@ import {
   GoCommonPresetOptions
 } from '../../../../src/generators';
 
-describe('Go_COMMON_PRESET', () => {
-  let generator: GoGenerator;
-  beforeEach(() => {
-    generator = new GoGenerator({
-      presets: [{ preset: GO_COMMON_PRESET, options: { addJsonTag: true } }]
+describe('GO_COMMON_PRESET', () => {
+  const enumDoc = {
+    type: 'string',
+    enum: ['2.6.0']
+  };
+
+  const structDoc = {
+    type: 'object',
+    properties: {
+      stringProp: { type: 'string' },
+      numberProp: { type: 'number' },
+      booleanProp: { type: 'boolean' }
+    }
+  };
+
+  test('should render json tags for structs', async () => {
+    const options: GoCommonPresetOptions = { addJsonTag: true };
+    const generator = new GoGenerator({
+      presets: [{ preset: GO_COMMON_PRESET, options }]
     });
+    const models = await generator.generate(structDoc);
+    expect(models).toHaveLength(1);
+    expect(models[0].result).toMatchSnapshot();
   });
-  test('');
+
+  test('should render the marshal functions for enum', async () => {
+    const options: GoCommonPresetOptions = { addJsonTag: true };
+    const generator = new GoGenerator({
+      presets: [{ preset: GO_COMMON_PRESET, options }]
+    });
+    const models = await generator.generate(enumDoc);
+    expect(models).toHaveLength(1);
+    expect(models[0].result).toMatchSnapshot();
+  });
 });
