580d2c862b96d99b6e192e906eb365aa4f78ddae
Jonas Lagoni <jonas-lt@live.dk>
2024-03-04 21:34:42 +0100

    Revert "update implementation and test"
    This reverts commit 83a84680c0d9f21bf95858ff117fff9c57352749.

--- src/generators/python/constrainer/ConstantConstrainer.ts
+++ src/generators/python/constrainer/ConstantConstrainer.ts
@@ -1,53 +1,7 @@
-import {
-  ConstrainedMetaModel,
-  ConstrainedEnumModel,
-  ConstrainedMetaModelOptionsConst,
-  ConstrainedReferenceModel,
-  ConstrainedStringModel
-} from '../../../models';
 import { PythonConstantConstraint } from '../PythonGenerator';
 
-const getConstrainedEnumModelConstant = (args: {
-  constrainedMetaModel: ConstrainedMetaModel;
-  constrainedEnumModel: ConstrainedEnumModel;
-  constOptions: ConstrainedMetaModelOptionsConst;
-}) => {
-  const constrainedEnumValueModel = args.constrainedEnumModel.values.find(
-    (value) => value.originalInput === args.constOptions.originalInput
-  );
-
-  if (constrainedEnumValueModel) {
-    return `${args.constrainedMetaModel.type}.${constrainedEnumValueModel.key}`;
-  }
-};
-
 export function defaultConstantConstraints(): PythonConstantConstraint {
-  return ({ constrainedMetaModel }) => {
-    const constOptions = constrainedMetaModel.options.const;
-
-    if (!constOptions) {
-      return undefined;
-    }
-
-    if (
-      constrainedMetaModel instanceof ConstrainedReferenceModel &&
-      constrainedMetaModel.ref instanceof ConstrainedEnumModel
-    ) {
-      return getConstrainedEnumModelConstant({
-        constrainedMetaModel,
-        constrainedEnumModel: constrainedMetaModel.ref,
-        constOptions
-      });
-    } else if (constrainedMetaModel instanceof ConstrainedEnumModel) {
-      return getConstrainedEnumModelConstant({
-        constrainedMetaModel,
-        constrainedEnumModel: constrainedMetaModel,
-        constOptions
-      });
-    } else if (constrainedMetaModel instanceof ConstrainedStringModel) {
-      return `"${constOptions.originalInput}"`;
-    }
-
+  return () => {
     return undefined;
   };
 }
