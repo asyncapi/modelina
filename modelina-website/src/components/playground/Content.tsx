c5d4b7bdef75c3554b6e0138506bf59846512cfe
Adarsh <adarshrawat8304@gmail.com>
2024-03-22 06:48:45 +0000

    feat: added resizable property in components

--- modelina-website/src/components/playground/Content.tsx
+++ modelina-website/src/components/playground/Content.tsx
@@ -8,6 +8,7 @@ import CustomError from '../CustomError';
 import OutputNavigation from './OutputNavigation';
 import { OptionsNavigation } from './OptionsNavigation';
 import GeneratedModelsComponent from './GeneratedModels';
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 interface ContentProps {
   setNewConfig: (config: string, configValue: any, updateCode?: boolean) => void;
@@ -37,53 +38,58 @@ export const Content: FunctionComponent<ContentProps> = ({ setNewConfig, setNewQ
 
   return (
     <div className="h-full w-full flex">
-      {/* OPTIONS & EDITOR */}
-      <div className="h-full w-[50%] flex">
-        {
-          showOptions && <div className={`bg-[#1f2937] text-white h-full w-[100%] md:w-[40%]`}>
-            <OptionsNavigation setNewConfig={setNewConfig} />
-          </div>
-        }
-        <div className={`h-full ${showOptions ? "w-[60%]" : "w-full"}`}>
-          <div className="max-xl:col-span-2 xl:grid-cols-1 h-full">
-            <div className="h-full bg-code-editor-dark text-white rounded-b shadow-lg font-bold">
-              <MonacoEditorWrapper
-                value={input}
-                onChange={(_, change) => {
-                  setInput(change);
-                  generateNewCode(change);
-                }}
-                editorDidMount={() => {
-                  setLoaded({ ...loaded, editorLoaded: true });
-                }}
-                language="json"
-              />
+      <PanelGroup direction="horizontal">
+        {/* OPTIONS & EDITOR */}
+        <Panel className="flex flex-row" id="left-panel">
+          {
+            showOptions && 
+            <div className={`bg-[#1f2937] text-white h-full w-[100%] md:w-[40%]`}>
+              <OptionsNavigation setNewConfig={setNewConfig} />
+            </div>
+          }
+          <div className={`h-full w-full`}>
+            <div className="max-xl:col-span-2 xl:grid-cols-1 h-full">
+              <div className="h-full bg-code-editor-dark text-white rounded-b shadow-lg font-bold">
+                <MonacoEditorWrapper
+                  value={input}
+                  onChange={(_, change) => {
+                    setInput(change);
+                    generateNewCode(change);
+                  }}
+                  editorDidMount={() => {
+                    setLoaded({ ...loaded, editorLoaded: true });
+                  }}
+                  language="json"
+                />
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </Panel>
 
-      {/* OUTPUT NAVIGATION AND OUTPUTS */}
-      <div className="h-full w-[50%] flex">
-        {
-          showOutputNavigation && <div className='h-full w-[100%] md:w-[30%]'>
-            <OutputNavigation />
+        <PanelResizeHandle />
+
+        {/* OUTPUT NAVIGATION AND OUTPUTS */}
+        <Panel className="flex flex-row" id="right-panel" minSize={4}>  
+          {
+            showOutputNavigation && <div className='h-full w-[100%] md:w-[30%]'>
+              <OutputNavigation />
+            </div>
+          }
+          <div className={`h-full ${showOutputNavigation ? "w-[70%]" : "w-full"}`}>
+            <div className={`h-full`}>
+              {error ? (
+                <CustomError statusCode={statusCode} errorMessage={errorMessage} />
+              ) : (
+                <PlaygroundGeneratedContext.Provider
+                  value={PlaygroundGeneratedContextValue}
+                >
+                  <GeneratedModelsComponent setNewQuery={setNewQuery} />
+                </PlaygroundGeneratedContext.Provider>
+              )}
+            </div>
           </div>
-        }
-        <div className={`h-full ${showOutputNavigation ? "w-[70%]" : "w-full"}`}>
-          <div className={`h-full`}>
-            {error ? (
-              <CustomError statusCode={statusCode} errorMessage={errorMessage} />
-            ) : (
-              <PlaygroundGeneratedContext.Provider
-                value={PlaygroundGeneratedContextValue}
-              >
-                <GeneratedModelsComponent setNewQuery={setNewQuery} />
-              </PlaygroundGeneratedContext.Provider>
-            )}
-          </div>
-        </div>
-      </div>
+        </Panel>
+      </PanelGroup>
     </div>
   );
 };
