c1da6056411b05f0e838ee35844f5b8ff3ed7f48
Jonas Lagoni <jonas-lt@live.dk>
2024-04-22 11:01:49 +0200

    update setup

--- modelina-cli/scripts/renameModelinaPack.js
+++ modelina-cli/scripts/renameModelinaPack.js
@@ -4,5 +4,13 @@
 const path = require('path');
 const modelinaPkg = require(path.resolve(__dirname, '../../package.json')); 
 const fs = require('fs'); 
-fs.mkdirSync(path.resolve(__dirname, './modelina-package'));
+const { spawnSync } = require('child_process');
+
+const modelinaPackageDir = path.resolve(__dirname, './modelina-package');
+if (!fs.existsSync(modelinaPackageDir)){
+    fs.mkdirSync(modelinaPackageDir);
+}
+
 fs.renameSync(path.resolve(__dirname, `../asyncapi-modelina-${modelinaPkg.version}.tgz`), path.resolve(__dirname, `./modelina-package/asyncapi-modelina.tgz`));
+
+spawnSync(`cd ${__dirname}/../ && npm install ./scripts/modelina-package/asyncapi-modelina.tgz`)
