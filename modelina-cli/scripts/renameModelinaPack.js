950aec3d3a278dcd0d4bfde2b02518a7e3a42139
Jonas Lagoni <jonas-lt@live.dk>
2024-04-22 11:48:06 +0200

    update setup

--- modelina-cli/scripts/renameModelinaPack.js
+++ modelina-cli/scripts/renameModelinaPack.js
@@ -13,4 +13,13 @@ if (!fs.existsSync(modelinaPackageDir)){
 fs.renameSync(path.resolve(__dirname, `../asyncapi-modelina-${modelinaPkg.version}.tgz`), path.resolve(__dirname, `./modelina-package/asyncapi-modelina.tgz`));
 
 // Make sure we update the installed tarball as we will run into EINTEGRITY issues
-spawnSync(`cd ${__dirname}/../ && npm update @asyncapi/modelina`)
+console.log(`Removing old @asyncapi/modelina dependency to avoid EINTEGRITY issues`)
+var child = spawnSync(`npm`, ["install", "./scripts/modelina-package/asyncapi-modelina.tgz"], { encoding : 'utf8', cwd: `${__dirname}/../` });
+console.log("Process finished.");
+if(child.error) {
+    console.log("ERROR: ",child.error);
+}
+console.log("stdout: ",child.stdout);
+console.log("stderr: ",child.stderr);
+console.log("exist code: ",child.status);
+console.log(`Modelina installed into CLI`)
