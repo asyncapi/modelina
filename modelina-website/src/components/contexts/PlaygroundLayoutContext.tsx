0a603ffdf38006e8c67e2404af8594ddf02b00ef
jerensl <54782057+jerensl@users.noreply.github.com>
2024-07-18 23:28:22 +0800

    fix(sidebar-playground): fix sidebar button on mobile version

--- modelina-website/src/components/contexts/PlaygroundLayoutContext.tsx
+++ modelina-website/src/components/contexts/PlaygroundLayoutContext.tsx
@@ -1,6 +1,9 @@
+'use client';
+
+import { useMeasure } from '@uidotdev/usehooks';
 import type { Draft, Immutable } from 'immer';
 import { enableMapSet, produce } from 'immer';
-import { createContext, useContext, useReducer } from 'react';
+import { createContext, useContext, useEffect, useReducer } from 'react';
 import { IoOptionsOutline } from 'react-icons/io5';
 import { LuFileInput, LuFileOutput } from 'react-icons/lu';
 import { VscListSelection } from 'react-icons/vsc';
@@ -57,12 +60,14 @@ const sidebarItems: Map<string, ISidebarItem> = new Map([
   ]
 ]);
 
-type ActionType = { type: 'open-option', name: string };
+type IDeviceType = 'mobile' | 'tablet' | 'notebook' | 'desktop';
+
+type ActionType = { type: 'open-option', name: string } | { type: 'update-device', payload: IDeviceType } | { type: '' };
 type Dispatch = (action: ActionType) => void;
-type State = Immutable<{  open: string, sidebarItems: typeof sidebarItems }>;
+type State = Immutable<{  open: string, device: IDeviceType, sidebarItems: typeof sidebarItems }>;
 type PlaygroundtProviderProps = { children: React.ReactNode };
 
-const initialState: State = { open: 'general-options', sidebarItems };
+const initialState: State = { open: 'general-options', device: 'desktop', sidebarItems };
 
 const PlaygroundtLayoutContext = createContext<
   { state: State; dispatch: Dispatch } | undefined
@@ -72,14 +77,22 @@ const playgroundLayoutReducer = produce<State, [ActionType]>((draft: Draft<State
   enableMapSet();
 
   switch (action.type) {
+    case 'update-device': {
+      // eslint-disable-next-line no-param-reassign
+      draft.device = action.payload;
+      break;
+    }
     case 'open-option': {
       const findOpts = draft.sidebarItems.get(action.name);
+
+      const alwaysOpen = ['input-editor', 'output-editor', 'general-options'];
+      const isMobile = draft.device === 'mobile';
 
       if (!findOpts) {
         break;
       }
 
-      if (action.name === 'input-editor' || action.name === 'output-editor') {
+      if (alwaysOpen.includes(action.name) && isMobile) {
         if (draft.open === action.name) {
         // eslint-disable-next-line no-param-reassign
           draft.open = findOpts.name ?? draft.open;
@@ -116,23 +129,6 @@ const playgroundLayoutReducer = produce<State, [ActionType]>((draft: Draft<State
 });
 
 /**
- * This component to providing Plaground Layout State.
- *
- * @param {ReactNode} children The children element of the component.
- * @returns {ReactNode} A React element that share context state.
- */
-function PlaygroundLayoutProvider({ children }: PlaygroundtProviderProps) {
-  const [state, dispatch] = useReducer(playgroundLayoutReducer, initialState);
-  const value = { state, dispatch };
-
-  return (
-  <PlaygroundtLayoutContext.Provider value={value}>
-    {children}
-  </PlaygroundtLayoutContext.Provider>
-);
-}
-
-/**
  * This component to consume Plaground Layout State.
  *
  * @returns {ReactNode} A React element that return state and update the state.
@@ -145,6 +141,41 @@ function usePlaygroundLayout() {
   }
 
   return context;
+}
+
+/**
+ * This component to providing Plaground Layout State.
+ *
+ * @param {ReactNode} children The children element of the component.
+ * @returns {ReactNode} A React element that share context state.
+ */
+function PlaygroundLayoutProvider({ children }: PlaygroundtProviderProps) {
+  const [state, dispatch] = useReducer(playgroundLayoutReducer, initialState);
+  const value = { state, dispatch };
+
+  const [ref, { width }] = useMeasure();
+
+  useEffect(() => {
+    if (width  !== null) {
+      if (width < 768) {
+        dispatch({ type: 'update-device', payload: 'mobile' });
+      } else if (width <= 1024) {
+        dispatch({ type: 'update-device', payload: 'tablet' });
+      } else if (width <= 1280) {
+        dispatch({ type: 'update-device', payload: 'notebook' });
+      } else {
+        dispatch({ type: 'update-device', payload: 'desktop' });
+      }
+    }
+  }, [width]);
+
+  return (
+  <PlaygroundtLayoutContext.Provider  value={value}>
+    <div ref={ref}>
+      {children}
+    </div>
+  </PlaygroundtLayoutContext.Provider>
+);
 }
 
 export { PlaygroundLayoutProvider, usePlaygroundLayout };
--- modelina-website/src/components/playground/Sidebar.tsx
+++ modelina-website/src/components/playground/Sidebar.tsx
@@ -4,65 +4,7 @@ import React, { useCallback } from 'react';
 import { usePlaygroundLayout } from '../contexts/PlaygroundLayoutContext';
 import { Tooltip } from './Tooltip';
 
-// interface SidebarItem {
-//   name: string;
-//   title: string;
-//   isActive: boolean;
-//   isShow: boolean;
-//   mobileOnly: boolean;
-//   onClick: () => void;
-//   icon: React.ReactNode;
-//   tooltip: React.ReactNode;
-// }
-
 export const Sidebar: React.FunctionComponent = () => {
-  // const { setShowOptions, setShowOutputNavigation, setShowInputEditor, showInputEditor, showOptions } =
-  //   usePlaygroundContext();
-  // const sidebarItems: SidebarItem[] = [
-  //   // Input/Output Editor
-  //   {
-  //     name: 'input-editor',
-  //     title: 'Input Editor',
-  //     isActive: false,
-  //     isShow: true,
-  //     mobileOnly: true,
-  //     onClick: () => {
-  //       setShowInputEditor((prevShowOptions) => !prevShowOptions);
-  //       setShowOutputNavigation(false);
-  //       setShowOptions(false);
-  //     },
-  //     icon: showInputEditor ? <LuFileInput className='size-5' /> : <LuFileOutput className='size-5' />,
-  //     tooltip: `Show ${showInputEditor ? 'Input Editor' : 'Output Editor'}`
-  //   },
-  //   // Options
-  //   {
-  //     name: 'options',
-  //     title: 'Options',
-  //     isActive: showOptions,
-  //     isShow: true,
-  //     mobileOnly: false,
-  //     onClick: () => {
-  //       setShowOptions((prevShowOptions) => !prevShowOptions);
-  //       setShowOutputNavigation(false);
-  //     },
-  //     icon: <IoOptionsOutline className='size-5' />,
-  //     tooltip: 'Show or hide all the options'
-  //   },
-  //   // Output Explorer
-  //   {
-  //     name: 'outputExplorer',
-  //     title: 'Output',
-  //     isActive: false,
-  //     isShow: showInputEditor,
-  //     mobileOnly: false,
-  //     onClick: () => {
-  //       setShowOutputNavigation((prevShowOutputNavigation) => !prevShowOutputNavigation);
-  //       setShowOptions(false);
-  //     },
-  //     icon: <VscListSelection className='size-5' />,
-  //     tooltip: 'Show or hide the list of output models'
-  //   }
-  // ];
   const { state, dispatch } = usePlaygroundLayout();
 
   const sidebarItems = Array.from(state.sidebarItems.values());
@@ -72,25 +14,6 @@ export const Sidebar: React.FunctionComponent = () => {
   return (
     <div className='flex size-full flex-col justify-start border-r border-gray-700 bg-[#1f2937] shadow-lg'>
       <div className='flex flex-col'>
-        {/* {sidebarItems.map((item) => (
-          <Tooltip content={item.tooltip} placement='right' hideOnClick={true} key={item.name}>
-            <button
-              title={item.title}
-              onClick={() => item.onClick()}
-              className={clsx('border-box flex p-2 text-sm focus:outline-none', {
-                'md:hidden': item.mobileOnly,
-                'hidden md:block': !item.isShow
-              })}
-              type='button'
-            >
-              <div
-                className={item.isActive ? 'rounded bg-gray-900 p-2 text-white' : 'p-2 text-gray-700 hover:text-white'}
-              >
-                {item.icon}
-              </div>
-            </button>
-          </Tooltip>
-        ))} */}
         {sidebarItems.map((item) => (
           <Tooltip content={item.tooltip} placement='right' hideOnClick={true} key={item.name}>
             <button
@@ -98,9 +21,8 @@ export const Sidebar: React.FunctionComponent = () => {
               onClick={() => handleClick({ name: item.name })}
               className={clsx('border-box flex p-2 text-sm focus:outline-none', {
                 'md:hidden': item.devices === 'mobile'
-                // 'hidden md:block': !item.isShow
               })}
-              disabled={item.name === 'output-options' && state.open === 'general-options'}
+              disabled={item.name === 'output-options' && state.open === 'general-options' && state.device === 'mobile'}
               type='button'
             >
               <div
