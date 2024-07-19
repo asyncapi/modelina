903edaccbea81416d574c7d17482d4e8a11a2965
Tom de Vroomen <social@tomdevroomen.nl>
2024-07-19 10:59:40 +0200

    feat: Allow to use constraints from jakarta namespace for Java and Kotlin models
    Description
    The JAVA_CONSTRAINT_PRESET uses constraints from the javax namespace.
    Since Java 17 the constraints are moved to the jakarta namespace.
    Add an options to the JAVA_CONSTRAINT_PRESET to use the jakarta namespace for constraint imports.
    
    Related issue
    fixes: (#1807)[https://github.com/asyncapi/modelina/issues/1807]

--- test/generators/java/presets/ConstraintsPreset.spec.ts
+++ test/generators/java/presets/ConstraintsPreset.spec.ts
@@ -4,30 +4,71 @@ import {
 } from '../../../../src/generators';
 
 describe('JAVA_CONSTRAINTS_PRESET', () => {
-  let generator: JavaGenerator;
-  beforeEach(() => {
-    generator = new JavaGenerator({ presets: [JAVA_CONSTRAINTS_PRESET] });
-  });
+  const doc = {
+    $id: 'Clazz',
+    type: 'object',
+    properties: {
+      min_number_prop: { type: 'number', minimum: 0 },
+      max_number_prop: { type: 'number', exclusiveMaximum: 100 },
+      array_prop: { type: 'array', minItems: 2, maxItems: 3 },
+      string_prop: {
+        type: 'string',
+        pattern: '^\\w+("\\.\\w+)*$',
+        minLength: 3
+      }
+    },
+    required: ['min_number_prop', 'max_number_prop']
+  };
 
-  test('should render constraints annotations', async () => {
-    const doc = {
-      $id: 'Clazz',
-      type: 'object',
-      properties: {
-        min_number_prop: { type: 'number', minimum: 0 },
-        max_number_prop: { type: 'number', exclusiveMaximum: 100 },
-        array_prop: { type: 'array', minItems: 2, maxItems: 3 },
-        string_prop: {
-          type: 'string',
-          pattern: '^\\w+("\\.\\w+)*$',
-          minLength: 3
-        }
-      },
-      required: ['min_number_prop', 'max_number_prop']
-    };
+  test('should render javax constraints annotations by default', async () => {
+    const generator = new JavaGenerator({ presets: [JAVA_CONSTRAINTS_PRESET] });
     const expectedDependencies = [
       'import java.util.Map;',
       'import javax.validation.constraints.*;'
+    ];
+
+    const models = await generator.generate(doc);
+    expect(models).toHaveLength(1);
+    expect(models[0].result).toMatchSnapshot();
+    expect(models[0].dependencies).toEqual(expectedDependencies);
+  });
+
+  test('should render javax constraints annotations when configured', async () => {
+    const generator = new JavaGenerator({
+      presets: [
+        {
+          preset: JAVA_CONSTRAINTS_PRESET,
+          options: {
+            useJakarta: false
+          }
+        }
+      ]
+    });
+    const expectedDependencies = [
+      'import java.util.Map;',
+      'import javax.validation.constraints.*;'
+    ];
+
+    const models = await generator.generate(doc);
+    expect(models).toHaveLength(1);
+    expect(models[0].result).toMatchSnapshot();
+    expect(models[0].dependencies).toEqual(expectedDependencies);
+  });
+
+  test('should render jakarta constraints annotations from when configured', async () => {
+    const generator = new JavaGenerator({
+      presets: [
+        {
+          preset: JAVA_CONSTRAINTS_PRESET,
+          options: {
+            useJakarta: true
+          }
+        }
+      ]
+    });
+    const expectedDependencies = [
+      'import java.util.Map;',
+      'import jakarta.validation.constraints.*;'
     ];
 
     const models = await generator.generate(doc);
