aad20bcc6d9fac54e0d4c0607940728c9deec0a5
jonaslagoni <jonas-lt@live.dk>
2024-10-30 11:29:42 +0100

    adapt tests

--- test/runtime/runtime-csharp/runtime-csharp/test/models/newtonsoft/Address.cs
+++ test/runtime/runtime-csharp/runtime-csharp/test/models/newtonsoft/Address.cs
@@ -14,22 +14,28 @@ public class AddressTests
     [Test]
     public void TestSerializingFullModel()
     {
-        Address address = new();
-        NestedObject nestedObject = new();
+        TestObject address = new();
+        ObjectType nestedObject = new();
         nestedObject.Test = "test";
-        address.NestedObject = nestedObject;
-        address.Marriage = true;
-        address.Members = 2;
-        address.HouseNumber = 1;
+        address.ObjectType = nestedObject;
+        address.BooleanType = true;
+        address.NumberType = 1;
         address.ArrayType = new dynamic[] { 1, "test" };
-        address.EnumTest = EnumTest.TEST;
-        address.HouseType = HousingType.FLAT;
-        address.RoofType = TypeOfRoof.STRAW;
-        address.AdditionalProperties = new Dictionary<string, dynamic>();
-        address.AdditionalProperties.Add("test_not_used", 2);
+        address.EnumType = EnumType.CURLYLEFT_QUOTATION_TEST_QUOTATION_COLON_2_CURLYRIGHT;
+        address.ArrayTypeSimple = new string[] { "test", "test2" };
+        address.DictionaryType = new Dictionary<string, string>
+        {
+            { "test", "test" }
+        };
+        //address.TupleType = ("test", 1d);
+        address.UnionType = "test";
+        address.AdditionalProperties = new Dictionary<string, dynamic>
+        {
+            { "test", "test" }
+        };
 
         string actualJsonString = JsonConvert.SerializeObject(address);
-        string expectedJsonString = "{\"house_number\":1.0,\"marriage\":true,\"members\":2,\"array_type\":[1,\"test\"],\"nestedObject\":{\"test\":\"test\"},\"enumTest\":\"test\",\"houseType\":\"flat\",\"roofType\":\"straw\",\"test_not_used\":2}";
+        string expectedJsonString = "{\"number_type\":1.0,\"boolean_type\":true,\"union_type\":\"test\",\"array_type_simple\":[\"test\",\"test2\"],\"array_type\":[1,\"test\"],\"tuple_type\":{\"Item1\":\"test\",\"Item2\":1.0},\"object_type\":{\"test\":\"test\"},\"dictionary_type\":{\"test\":\"test\"},\"enum_type\":\"{\\\"test\\\":2}\",\"test\":\"test\"}";
         Assert.That(actualJsonString, Is.EqualTo(expectedJsonString));
     }
 }
