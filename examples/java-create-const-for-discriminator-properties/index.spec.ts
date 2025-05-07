8d4c232cd1ec6671721e7544d42df98d49223ec8
Thomas Verhoeven <thomas.verhoeven@hotmail.com>
2025-05-07 08:27:40 +0200

    feat(java-generator): Option to automatically fill in discriminator properties with their model name

---
+++ examples/java-create-const-for-discriminator-properties/index.spec.ts
@@ -0,0 +1,14 @@
+const spy = jest.spyOn(global.console, 'log').mockImplementation(() => {
+  return;
+});
+import { generate } from './index';
+
+describe('Should be able to generate const for discriminator properties', () => {
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+  test('and should log expected output to console', async () => {
+    await generate();
+    expect(spy.mock.calls[0]).toMatchSnapshot();
+  });
+});
