9ec1b248e3af5ea115349e7bbd398dcdff08756c
Joseph Tarbit <joetarbit@gmail.com>
2025-05-07 21:21:01 +0100

    fix: only import List if a list is present for Java (#2248)

--- src/generators/java/renderers/ClassRenderer.ts
+++ src/generators/java/renderers/ClassRenderer.ts
@@ -1,5 +1,6 @@
 import { JavaRenderer } from '../JavaRenderer';
 import {
+  ConstrainedArrayModel,
   ConstrainedDictionaryModel,
   ConstrainedObjectModel,
   ConstrainedObjectPropertyModel,
@@ -26,7 +27,10 @@ export class ClassRenderer extends JavaRenderer<ConstrainedObjectModel> {
       await this.runAdditionalContentPreset()
     ];
 
-    if (this.options?.collectionType === 'List') {
+    if (
+      this.model.containsPropertyType(ConstrainedArrayModel) &&
+      this.options?.collectionType === 'List'
+    ) {
       this.dependencyManager.addDependency('import java.util.List;');
     }
     if (this.model.containsPropertyType(ConstrainedDictionaryModel)) {
--- src/generators/java/renderers/RecordRenderer.ts
+++ src/generators/java/renderers/RecordRenderer.ts
@@ -1,5 +1,6 @@
 import { JavaRenderer } from '../JavaRenderer';
 import {
+  ConstrainedArrayModel,
   ConstrainedDictionaryModel,
   ConstrainedObjectModel,
   ConstrainedObjectPropertyModel,
@@ -21,7 +22,10 @@ export class RecordRenderer extends JavaRenderer<ConstrainedObjectModel> {
       await this.runAdditionalContentPreset()
     ];
 
-    if (this.options?.collectionType === 'List') {
+    if (
+      this.model.containsPropertyType(ConstrainedArrayModel) &&
+      this.options?.collectionType === 'List'
+    ) {
       this.dependencyManager.addDependency('import java.util.List;');
     }
     if (this.model.containsPropertyType(ConstrainedDictionaryModel)) {
