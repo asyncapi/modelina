7fab703867ce4eaf641eacb3eef72752713a241c
Varad Raj Agrawal <agrawalvaradraj2007@gmail.com>
2026-01-20 17:26:39 +0530

    fix: stabilize Kotlin, Scala, and Rust runtime tests (#2415)

--- test/runtime/runtime-scala.spec.ts
+++ test/runtime/runtime-scala.spec.ts
@@ -1,19 +1,21 @@
 import { execCommand } from '../TestUtils/GeneralUtils';
 import path from 'path';
 
-jest.setTimeout(50000);
+jest.setTimeout(500000);
 
 test('Scala runtime testing', async () => {
+  const generateCommand = 'npm run generate:runtime:scala';
+  await execCommand(generateCommand, true);
   //The 'build' command here
   const buildCommand = `cd ${path.resolve(
     __dirname,
     './runtime-scala'
-  )} && ./gradlew build`;
-  await execCommand(buildCommand);
+  )} && ./gradlew clean build`;
+  await execCommand(buildCommand, true);
   //The 'test' command here
   const testCommand = `cd ${path.resolve(
     __dirname,
     './runtime-scala'
   )} && ./gradlew test`;
-  await execCommand(testCommand);
+  await execCommand(testCommand, true);
 });
